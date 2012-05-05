b3aecc0d03943abd79c9a240d49f1f488d453ad7
Tony Findeisen <github@it-ony.de>
2012-05-05 11:57:01 +0200

    added Product Model

--- sprd/model/Product.js
+++ sprd/model/Product.js
@@ -1,6 +1,6 @@
 define(["sprd/data/SprdModel"], function (SprdModel) {
-    return SprdModel.inherit("sprd.data.Article",{
-        price: function() {
+    return SprdModel.inherit("sprd.model.Product", {
+        price: function () {
             // TODO format price with currency
             return this.$.price.vatIncluded;
         }
