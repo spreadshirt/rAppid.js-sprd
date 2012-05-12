a3410ac523f72bfd5ac641c86814a88cec7256d9
Marcus Krejpowicz <krebbl@gmail.com>
2012-05-12 15:18:32 +0200

    added method to getAppearanceById

--- sprd/model/ProductType.js
+++ sprd/model/ProductType.js
@@ -1,15 +1,35 @@
 define(["sprd/data/SprdModel"], function (SprdModel) {
     return SprdModel.inherit("sprd.model.ProductType", {
         getDefaultView: function () {
-            var defaultViewId = this.$.defaultValues.defaultView;
-            var view;
-            for (var i = 0; i < this.$.views.$items.length; i++) {
-                view = this.$.views.$items[i];
-                if (defaultViewId == view.id) {
-                    return view;
+            if (this.$.defaultValues) {
+                var defaultViewId = this.$.defaultValues.defaultView.id;
+                var view;
+                for (var i = 0; i < this.$.views.$items.length; i++) {
+                    view = this.$.views.$items[i];
+                    if (defaultViewId === view.id) {
+                        return view;
+                    }
                 }
             }
             return this.$.views.$items[0];
+        },
+        getDefaultAppearance: function () {
+            if (this.$.defaultValues) {
+                return this.getAppearanceById(this.$.defaultValues.defaultAppearance.id);
+            }
+            return null;
+        },
+        getAppearanceById: function(id){
+            if(this.$.appearances){
+                var app;
+                for (var i = 0; i < this.$.appearances.$items.length; i++) {
+                    app = this.$.appearances.$items[i];
+                    if (id === app.id) {
+                        return app;
+                    }
+                }
+            }
+            return null;
         }
-    });
+    })
 });
