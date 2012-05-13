f2bc5fc894b8403261d110863f77c77ad414a7d4
Marcus Krejpowicz <krebbl@gmail.com>
2012-05-13 20:55:50 +0200

    fixed format Price method

--- sprd/model/Currency.js
+++ sprd/model/Currency.js
@@ -1,17 +1,27 @@
 define(["sprd/data/SprdModel"], function (Model) {
     return Model.inherit('sprd.model.Currency', {
-        formatPrice: function (val) {
-            val = new String(val);
-            if(val.split(".").pop().length < this.$.decimalCount){
-                val += "0";
+        formatPrice: function (price, type) {
+            if(!price) {
+                return null;
             }
-
+            type = type || "vatIncluded";
+            return this.formatValue(price[type]);
+        },
+        formatValue: function(val){
+            val = new String(val).split(".");
+            var len = val.length;
+            if (len === 1) {
+                val.push("00");
+            } else if (val[1].length < this.$.decimalCount) {
+                val[1] += "0";
+            } else if(val[1].length > this.$.decimalCount) {
+                val[1] = val[1].substr(0,2);
+            }
             if (this.$.pattern) {
-                return this.$.pattern.replace('%', val).replace('$', this.$.symbol);
+                return this.$.pattern.replace('%', val.join(",")).replace('$', this.$.symbol).replace('.', ',');
             } else {
                 return val;
             }
-
         }
     });
 
