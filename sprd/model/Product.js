6e593fe03f3ec1208e439cbedde1d8cdddc44b64
Marcus Krejpowicz <krebbl@gmail.com>
2012-05-13 20:59:03 +0200

    added getter for default view

--- sprd/model/Product.js
+++ sprd/model/Product.js
@@ -3,6 +3,12 @@ define(["sprd/data/SprdModel"], function (SprdModel) {
         price: function () {
             // TODO format price with currency
             return this.$.price.vatIncluded;
+        },
+        getDefaultView: function(){
+            if(this.$.defaultValues && this.$.productType) {
+                return this.$.productType.getViewById(this.$.defaultValues.defaultView.id);
+            }
+            return null;
         }
     });
 });
