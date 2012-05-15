9facbedf9a506abde34ca831e47b4c15da33a613
Marcus Krejpowicz <krebbl@gmail.com>
2012-05-15 18:48:27 +0200

    added designer

---
+++ sprd/view/Designer.js
@@ -0,0 +1,47 @@
+define(['js/ui/View','sprd/model/Product', 'Raphael'], function(View, Product, Raphael){
+
+    return View.inherit('sprd.view.Designer',{
+        $classAttributes: ['productType','product','appearance'],
+        defaults: {
+            height: 300,
+            width: 300,
+            view: null
+        },
+        ctor: function () {
+            this.callBase();
+
+            this.$.product = this.$.product || new Product();
+        },
+        render: function(){
+            var el = this.callBase();
+
+            return el;
+        },
+        _renderAppearance: function(appearance){
+            // TODO: implement
+        },
+        _renderView: function(view, oldView){
+            if(view){
+                if (!this.$paper) {
+                    this.$paper = Raphael(this.$el, view.$.size.width, view.$.size.height);
+                }
+
+                this.$paper.clear();
+                this.$paper.width = view.$.size.width;
+                this.$paper.height = view.$.size.height;
+
+                this.$printAreas = [];
+                // for each print area
+                for(var i = 0 ; i < view.$.viewMaps.length; i++){
+                    this.$printAreas.push(this._createPrintArea(view.$.viewMaps[i]));
+                }
+
+            }
+        },
+        _createPrintArea: function(printArea){
+            return this.$paper.rect(printArea.offset.x,printArea.offset.y, printArea.boundary.size.width, printArea.boundary.size.height);
+        }
+    });
+
+
+});
