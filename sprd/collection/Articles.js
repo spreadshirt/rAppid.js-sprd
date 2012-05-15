b9a8f535442a42daec8552f2e0cb4eab7fafa1de
Tony Findeisen <github@it-ony.de>
2012-05-15 20:01:49 +0200

    ArticlesCollection

---
+++ sprd/collection/Articles.js
@@ -0,0 +1,6 @@
+define(["js/data/Collection"], function(Collection) {
+    return Collection.inherit("sprd.collection.Articles", {
+        // TODO: use this collection during reference resolving
+        // TODO: always fetch currency with article
+    });
+});
