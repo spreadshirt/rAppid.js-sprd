42cf48fd5c2c32a6fd636dc0fd5ead3d0a4834a7
Tony Findeisen <github@it-ony.de>
2012-05-19 13:23:48 +0200

    moved List to defaults

--- sprd/model/Product.js
+++ sprd/model/Product.js
@@ -1,27 +1,20 @@
 define(["sprd/data/SprdModel", 'js/core/List'], function (SprdModel, List) {
-	return SprdModel.inherit("sprd.model.Product", {
+    return SprdModel.inherit("sprd.model.Product", {
 
-		defaults : {
-			productType : null
-		},
+        defaults: {
+            productType: null,
+            configurations: List
+        },
 
-		initialize : function () {
-
-			this.set('configurations', new List());
-
-			this.callBase();
-
-		},
-
-		price          : function () {
-			// TODO format price with currency
-			return this.$.price.vatIncluded;
-		},
-		getDefaultView : function () {
-			if (this.$.defaultValues && this.$.productType) {
-				return this.$.productType.getViewById(this.$.defaultValues.defaultView.id);
-			}
-			return null;
-		}
-	});
+        price: function () {
+            // TODO format price with currency
+            return this.$.price.vatIncluded;
+        },
+        getDefaultView: function () {
+            if (this.$.defaultValues && this.$.productType) {
+                return this.$.productType.getViewById(this.$.defaultValues.defaultView.id);
+            }
+            return null;
+        }
+    });
 });
