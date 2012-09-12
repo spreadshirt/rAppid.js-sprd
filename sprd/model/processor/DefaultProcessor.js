9d165bc7db045f6b9c2bd1ea02cc767e6ec24b68
Marcus Krejpowicz <krebbl@gmail.com>
2012-09-12 18:27:57 +0200

    fixed href

--- sprd/model/processor/DefaultProcessor.js
+++ sprd/model/processor/DefaultProcessor.js
@@ -1,12 +1,12 @@
 define(['js/data/DataSource'], function (DataSource) {
 
     return DataSource.Processor.inherit("sprd.model.processor.DefaultProcessor", {
-        _composeSubModel: function(model, action, options) {
+        _composeSubModel: function (model, action, options) {
             // we do not want to compose sub models, but rather return a reference to the model
             return {
                 id: model.$.id,
                 // TODO: determinate href
-                href: "foo"
+                href: model.$.href
             }
         }
     });
