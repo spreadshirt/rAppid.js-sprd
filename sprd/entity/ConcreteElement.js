2a099d5f3f01298bde0ce4b8fe901724d170d7ea
Tony Findeisen <github@it-ony.de>
2012-10-01 17:32:23 +0200

    added sku method to generate a unique sku id

--- sprd/entity/ConcreteElement.js
+++ sprd/entity/ConcreteElement.js
@@ -1,22 +1,25 @@
-define(["js/data/Entity", "sprd/model/Product", "sprd/model/Article"], function (Entity, Product, Article) {
+define(["js/data/Entity", "sprd/model/Product", "sprd/model/Article", "sprd/entity/Appearance"], function (Entity, Product, Article, Appearance) {
     var TYPE_PRODUCT = "sprd:product";
     var TYPE_ARTICLE = "sprd:article";
 
     return Entity.inherit("sprd.entity.ConcreteElement",{
         $schema: {
-            appearance: Entity,
+            appearance: Appearance,
             size: Entity
         },
+
         defaults: {
             appearance: null,
             size: null,
             item: null
         },
+
         isEqual: function(concreteElement){
             return this.get('appearance.id') === concreteElement.get('appearance.id') &&
                 this.get('size.id')  === concreteElement.get('size.id') &&
                 this.get('item.id') === concreteElement.get('item.id');
         },
+
         getProduct: function(){
             if(this.$.item instanceof Product){
                 return this.$.item;
@@ -25,12 +28,14 @@ define(["js/data/Entity", "sprd/model/Product", "sprd/model/Article"], function 
             }
             return null;
         }.on(['item','change:product']),
+
         getArticle: function() {
             if (this.$.item instanceof Article) {
                 return this.$.item;
             }
             return null;
         },
+
         getType: function(){
             if (this.$.item instanceof Product) {
                 return TYPE_PRODUCT;
@@ -38,6 +43,12 @@ define(["js/data/Entity", "sprd/model/Product", "sprd/model/Article"], function 
                 return TYPE_ARTICLE;
             }
             return null;
+        },
+
+        sku: function() {
+            return [ (this.getType() === TYPE_PRODUCT ? 'P' : 'A') + this.get('item.id'),
+                this.get('appearance.id'),
+                this.get('size.id')].join(' - ');
         }
 
     });
