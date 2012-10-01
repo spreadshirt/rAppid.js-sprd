a39c9fc184e651a3cd0841a7d7c40809f8da5b4b
Tony Findeisen <github@it-ony.de>
2012-10-01 17:32:38 +0200

    returns between method names

--- sprd/model/BasketItem.js
+++ sprd/model/BasketItem.js
@@ -10,28 +10,36 @@ define(["sprd/data/SprdModel", "sprd/entity/ConcreteElement"], function (SprdMod
             element: null,
             price: null
         },
+
         _commitElement: function(){
             this.set('price', this.get('element.item.price'), {silent: true});
         },
+
         increaseQuantity: function(quantity) {
             this.set('quantity', quantity + this.$.quantity);
         },
+
         _updatePrices: function(){
             this.totalVatIncluded.trigger();
             this.totalVatExcluded.trigger();
         },
+
         vatIncluded: function(){
             return this.get('price.vatIncluded') || 0;
         },
+
         vatExcluded: function(){
             return this.get('price.vatExcluded') || 0;
         },
+
         totalVatIncluded: function(){
             return this.vatIncluded() * this.$.quantity;
         }.on('change:quantity'),
+
         totalVatExcluded: function () {
             return this.vatExcluded() * this.$.quantity;
         }.on('change:quantity'),
+
         prepare: function(){
             return {
                 quantity: this.$.quantity,
