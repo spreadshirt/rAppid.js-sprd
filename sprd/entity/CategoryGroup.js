49ab399d87040872b985203b7baaaaf9a4ec7096
Marcus Krejpowicz <krebbl@gmail.com>
2012-10-10 16:50:14 +0200

    added category group

---
+++ sprd/entity/CategoryGroup.js
@@ -0,0 +1,29 @@
+define(['js/core/List'], function(List){
+
+    return List.inherit('sprd.entity.CategoryGroup', {
+        includesDepartment: function(department){
+            return !department || this.each(function(category){
+                if(category.$parent === department){
+                    this.return(true);
+                }
+            }) || false;
+        },
+        getCategoryByDepartment: function(department){
+            var ret = new List();
+            this.each(function (category) {
+                if (category.$parent === department) {
+                    ret.add(category);
+                }
+            });
+            return ret;
+        },
+        getId: function(){
+            var id = [];
+            this.each(function (category) {
+                id.push(category.$.id);
+            });
+            return id.join("");
+        }
+    });
+
+});
