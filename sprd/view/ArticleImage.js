b6c151c8535aff3cb4eebad346d2d06d558a21b1
Tony Findeisen <github@it-ony.de>
2012-10-17 13:36:33 +0200

    lazy refresh article image

--- sprd/view/ArticleImage.js
+++ sprd/view/ArticleImage.js
@@ -1,18 +1,20 @@
 define(["sprd/view/ProductImage"], function (ProductImage) {
 
     return ProductImage.inherit("sprd.view.ProductImage", {
+
         $classAttributes: ["article"],
-        imageUrl: function(){
+
+        imageUrl: function () {
             var url;
-            if(this.$.article){
+            if (this.$.article && this.$.article.$.resources && this.$.article.$.resources.length) {
                 url = this.$.article.$.resources[0].href;
 
                 var id = this.$.article.$.id;
                 id = Math.floor(parseInt(id.substr(id.length - 2)) / 5);
-                url = url.replace(/:\/\/image/,'://image'+id);
+                url = url.replace(/:\/\/image/, '://image' + id);
 
-                if(this.$.type === ProductImage.TYPE_COMPOSITION){
-                    url = url.replace("products","compositions");
+                if (this.$.type === ProductImage.TYPE_COMPOSITION) {
+                    url = url.replace("products", "compositions");
                 }
 
                 url = this.extendUrlWithSizes(url);
@@ -20,13 +22,11 @@ define(["sprd/view/ProductImage"], function (ProductImage) {
                 return url;
             }
             return this.callBase();
-        }.onChange('article'),
-        alt: function () {
-            if (this.$.article) {
-                return this.$.article.$.name;
-            }
+        }.onChange("article")
+            .on(["article", "change:resources"]),
 
-            return "";
+        alt: function () {
+            return this.get("article.name");
         }
 
     });
