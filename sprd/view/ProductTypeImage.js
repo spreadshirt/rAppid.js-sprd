20a5038f02e259017fed869ee156d255e176ddb5
Marcus Krejpowicz <krebbl@gmail.com>
2012-11-20 13:52:38 +0100

    added size type

--- sprd/view/ProductTypeImage.js
+++ sprd/view/ProductTypeImage.js
@@ -1,23 +1,32 @@
 define([
-	'xaml!sprd/view/Image'
+    'xaml!sprd/view/Image'
 ], function (Image) {
 
-	return Image.inherit('app.view.ProductTypeImage', {
+    return Image.inherit('app.view.ProductTypeImage', {
 
-		imageUrl : function () {
-			if (this.$.productType && this.$.productType.$.resources) {
-				return this.extendUrlWithSizes(this.$.productType.$.resources[0].href);
-			}
-			return "";
-		}.onChange('width', 'height', 'productType'),
+        defaults: {
+            type: "preview"
+        },
+        imageUrl: function () {
+            if (this.$.productType && this.$.productType.$.resources) {
+                var url;
+                if (this.$.type === "preview") {
+                    url = this.$.productType.$.resources[0].href;
+                } else if (this.$.type === "size") {
+                    url = this.$.productType.$.resources[1].href;
+                }
+                return this.extendUrlWithSizes(url);
+            }
+            return "";
+        }.onChange('width', 'height', 'productType'),
 
-		alt : function () {
-			if (this.$.productType) {
-				return this.$.productType.$.name;
-			}
-			return null;
-		}
+        alt: function () {
+            if (this.$.productType) {
+                return this.$.productType.$.name;
+            }
+            return null;
+        }
 
-	});
+    });
 
 });
