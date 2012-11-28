3d07d00b85bff0560abf397378f8c343f5c4021e
Tony Findeisen <github@it-ony.de>
2012-11-28 17:36:08 +0100

    added signature calculation

--- sprd/data/SprdApiDataSourceClass.js
+++ sprd/data/SprdApiDataSourceClass.js
@@ -1,5 +1,5 @@
-define(["js/data/RestDataSource", "underscore", "sprd/data/SprdModel", "sprd/model/processor/DefaultProcessor", "sprd/model/processor/BasketProcessor", "sprd/model/processor/BasketItemProcessor"],
-    function (RestDataSource, _, SprdModel, DefaultProcessor, BasketProcessor, BasketItemProcessor) {
+define(["js/data/RestDataSource", "underscore", "sprd/data/SprdModel", "sprd/model/processor/DefaultProcessor", "sprd/model/processor/BasketProcessor", "sprd/model/processor/BasketItemProcessor", "js/data/Model", "js/data/Collection"],
+    function (RestDataSource, _, SprdModel, DefaultProcessor, BasketProcessor, BasketItemProcessor, Model, Collection) {
 
         var SprdApiDataSource = RestDataSource.inherit('sprd.data.SprdApiDataSource', {
 
@@ -7,6 +7,9 @@ define(["js/data/RestDataSource", "underscore", "sprd/data/SprdModel", "sprd/mod
                 locale: "en_EU",
                 parsePayloadOnCreate: false,
                 parsePayloadOnUpdate: false,
+                apiKey: null,
+                secret: null,
+                session: null
             },
 
             $defaultProcessorFactory: DefaultProcessor,
@@ -16,19 +19,59 @@ define(["js/data/RestDataSource", "underscore", "sprd/data/SprdModel", "sprd/mod
                 BasketItemProcessor: BasketItemProcessor
             },
 
-            getQueryParameter: function (action) {
+            getQueryParameter: function (method, resource) {
                 var params = _.defaults({
                     mediaType: "json",
                     locale: this.$.locale
-                }, this.callBase(action));
+                }, this.callBase(method));
 
-                if (this.$.apiKey) {
+                var configuration,
+                apiKey = this.$.apiKey;
+
+                if (this.$.session) {
+                    // get the configuration only if needed
+                    configuration = this._getConfigurationForResource(resource);
+
+                    if (!configuration) {
+                        throw new Error("Configuration for resource not found");
+                    }
+
+                    if (configuration.$.sessionRequired) {
+
+                        var secret = this.$.secret;
+                        var sessionId = this.$.session.$.id;
+                        var time = Date.now();
+                        var url = this._buildUriForResource(resource, this.$.endPoint);
+
+                        var data = method + " " + url + " " + time + " " + secret;
+
+                        params = _.defaults({
+                            sig: SprdApiDataSource.SHA1(data),
+                            time: time,
+                            sessionId: sessionId,
+                            apiKey: apiKey
+                        });
+                    }
+                }
+
+                if (apiKey) {
                     params = _.defaults(params, {
-                        apiKey: this.$.apiKey
+                        apiKey: apiKey
                     });
                 }
 
                 return params;
+            },
+
+            _getConfigurationForResource: function(resource) {
+
+                if (resource instanceof Model) {
+                    return this.$dataSourceConfiguration.getConfigurationForModelClass(resource.factory);
+                } else if (resource instanceof Collection) {
+                    return this.$dataSourceConfiguration.getConfigurationForModelClass(resource.$modelFactory);
+                }
+
+                return null;
             },
 
             createContext: function (properties, parentContext) {
@@ -98,6 +141,155 @@ define(["js/data/RestDataSource", "underscore", "sprd/data/SprdModel", "sprd/mod
                 return _.defaults(parameter, this.callBase());
             }
         });
+
+        SprdApiDataSource.SHA1 = function SHA1(msg) {
+            function rotate_left(n, s) {
+                return ( n << s ) | (n >>> (32 - s));
+            }
+
+            function cvt_hex(val) {
+                var str = "";
+                var i;
+                var v;
+
+                for (i = 7; i >= 0; i--) {
+                    v = (val >>> (i * 4)) & 0x0f;
+                    str += v.toString(16);
+                }
+                return str;
+            }
+
+            function Utf8Encode(string) {
+                string = string.replace(/\r\n/g, "\n");
+                var utfText = "";
+
+                for (var n = 0; n < string.length; n++) {
+
+                    var c = string.charCodeAt(n);
+
+                    if (c < 128) {
+                        utfText += String.fromCharCode(c);
+                    }
+                    else if ((c > 127) && (c < 2048)) {
+                        utfText += String.fromCharCode((c >> 6) | 192);
+                        utfText += String.fromCharCode((c & 63) | 128);
+                    }
+                    else {
+                        utfText += String.fromCharCode((c >> 12) | 224);
+                        utfText += String.fromCharCode(((c >> 6) & 63) | 128);
+                        utfText += String.fromCharCode((c & 63) | 128);
+                    }
+
+                }
+
+                return utfText;
+            }
+
+            var blockStart;
+            var i, j;
+            var W = new Array(80);
+            var H0 = 0x67452301;
+            var H1 = 0xEFCDAB89;
+            var H2 = 0x98BADCFE;
+            var H3 = 0x10325476;
+            var H4 = 0xC3D2E1F0;
+            var A, B, C, D, E;
+            var temp;
+
+            msg = Utf8Encode(msg);
+
+            var msg_len = msg.length;
+
+            var word_array = new Array();
+            for (i = 0; i < msg_len - 3; i += 4) {
+                j = msg.charCodeAt(i) << 24 | msg.charCodeAt(i + 1) << 16 |
+                    msg.charCodeAt(i + 2) << 8 | msg.charCodeAt(i + 3);
+                word_array.push(j);
+            }
+
+            switch (msg_len % 4) {
+                case 0:
+                    i = 0x080000000;
+                    break;
+                case 1:
+                    i = msg.charCodeAt(msg_len - 1) << 24 | 0x0800000;
+                    break;
+
+                case 2:
+                    i = msg.charCodeAt(msg_len - 2) << 24 | msg.charCodeAt(msg_len - 1) << 16 | 0x08000;
+                    break;
+
+                case 3:
+                    i = msg.charCodeAt(msg_len - 3) << 24 | msg.charCodeAt(msg_len - 2) << 16 | msg.charCodeAt(msg_len - 1) << 8 | 0x80;
+                    break;
+            }
+
+            word_array.push(i);
+
+            while ((word_array.length % 16) != 14) word_array.push(0);
+
+            word_array.push(msg_len >>> 29);
+            word_array.push((msg_len << 3) & 0x0ffffffff);
+
+
+            for (blockStart = 0; blockStart < word_array.length; blockStart += 16) {
+
+                for (i = 0; i < 16; i++) W[i] = word_array[blockStart + i];
+                for (i = 16; i <= 79; i++) W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
+
+                A = H0;
+                B = H1;
+                C = H2;
+                D = H3;
+                E = H4;
+
+                for (i = 0; i <= 19; i++) {
+                    temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
+                    E = D;
+                    D = C;
+                    C = rotate_left(B, 30);
+                    B = A;
+                    A = temp;
+                }
+
+                for (i = 20; i <= 39; i++) {
+                    temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
+                    E = D;
+                    D = C;
+                    C = rotate_left(B, 30);
+                    B = A;
+                    A = temp;
+                }
+
+                for (i = 40; i <= 59; i++) {
+                    temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
+                    E = D;
+                    D = C;
+                    C = rotate_left(B, 30);
+                    B = A;
+                    A = temp;
+                }
+
+                for (i = 60; i <= 79; i++) {
+                    temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
+                    E = D;
+                    D = C;
+                    C = rotate_left(B, 30);
+                    B = A;
+                    A = temp;
+                }
+
+                H0 = (H0 + A) & 0x0ffffffff;
+                H1 = (H1 + B) & 0x0ffffffff;
+                H2 = (H2 + C) & 0x0ffffffff;
+                H3 = (H3 + D) & 0x0ffffffff;
+                H4 = (H4 + E) & 0x0ffffffff;
+
+            }
+
+            temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
+            return temp.toLowerCase();
+        };
 
 
         return SprdApiDataSource;
