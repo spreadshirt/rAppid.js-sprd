acaffbea4c57e96a09d8b46002b70ac6a6c31354
Tony Findeisen <github@it-ony.de>
2012-11-30 18:28:21 +0100

    extract information from file image object

--- sprd/entity/FileSystemImage.js
+++ sprd/entity/FileSystemImage.js
@@ -1,8 +1,26 @@
-define(['sprd/entity/Image'], function(Image) {
+define(['sprd/entity/Image', 'underscore'], function(Image, _) {
 
     return Image.inherit('sprd/entity/FileSystemImage', {
         defaults: {
             file: null
+        },
+
+        ctor: function(attributes) {
+            attributes = attributes || {};
+
+            var file = attributes.file;
+
+            if (file) {
+                _.defaults(attributes, {
+                    name: file.name,
+                    lastModifiedDate: file.lastModifiedDate,
+                    type: file.type,
+                    size: file.size
+                });
+            }
+
+            this.callBase(attributes);
+
         }
     });
 });
