de4544fdf874f2f37f1fbac6c39600359c8dc27a
Tony Findeisen <github@it-ony.de>
2012-11-30 18:28:03 +0100

    Created ImageServerDataSource

---
+++ sprd/data/ImageServerDataSourceClass.js
@@ -0,0 +1,57 @@
+define(["sprd/data/SprdDataSource", "js/data/Model", "js/data/RestDataSource", "js/data/DataSource"],
+    function (SprdDataSource, Model, RestDataSource, DataSource) {
+
+        var _formatProcessorCache = {};
+
+        var ImageServerDataSourceClass = SprdDataSource.inherit('sprd.data.ImageServerDataSourceClass', {
+
+            defaults: {
+                parsePayloadOnUpdate: false
+            },
+
+            _saveModel: function (model, options, callback) {
+
+                if (model._status() !== Model.STATE.CREATED) {
+                    callback("Model isn't created");
+                    return;
+                }
+
+                this.callBase();
+
+            },
+
+            getFormatProcessor: function (action, model) {
+
+                var type = model.$.file.type;
+
+                if (!_formatProcessorCache[type]) {
+                    _formatProcessorCache[type] = new ImageServerDataSourceClass.ImageFormatProcessor(type);
+                }
+
+                return _formatProcessorCache[type];
+            },
+            getQueryParameter: function (method, model) {
+                return _.defaults({mediaType: model.$.file.type.split("/").pop()}, this.callBase());
+            }
+        });
+
+        ImageServerDataSourceClass.ImageFormatProcessor = DataSource.FormatProcessor.inherit({
+
+            ctor: function (type) {
+                this.$type = type;
+                this.callBase();
+            },
+
+            serialize: function (data) {
+                var ret = new FormData();
+                ret.append('upload_field', data.file);
+                return ret;
+            },
+
+            getContentType: function () {
+                return false;
+            }
+        });
+
+        return ImageServerDataSourceClass;
+    });
