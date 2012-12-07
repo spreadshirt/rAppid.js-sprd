bcd8a7fbff0d9153785b25df06dd4b0fda940797
Marcus Krejpowicz <krebbl@gmail.com>
2012-12-07 14:29:42 +0100

    Add abstract shop

---
+++ sprd/model/AbstractShop.js
@@ -0,0 +1,41 @@
+define([
+    "sprd/data/SprdModel",
+    'js/data/Collection',
+    'sprd/model/Basket',
+    'sprd/model/Currency',
+    'sprd/model/Design',
+    'sprd/model/ProductType',
+    'sprd/model/ProductTypeDepartment',
+    'sprd/model/ArticleCategory',
+    'sprd/model/Article', 'sprd/model/Country', 'sprd/model/PrintType', 'sprd/model/FontFamily', "sprd/model/Product"],
+    function (SprdModel, Collection, Basket, Currency, Design, ProductType, ProductTypeDepartment, ArticleCategory, Article, Country, PrintType, FontFamily, Product) {
+
+        return SprdModel.inherit("sprd.model.Shop", {
+            schema: {
+
+
+                country: Country,
+//            language: SprdModel,
+                currency: Currency,
+//            address: SprdModel,
+
+                productTypes: Collection.of(ProductType),
+                printTypes: Collection.of(PrintType),
+                fontFamilies: Collection.of(FontFamily),
+                productTypeDepartments: Collection.of(ProductTypeDepartment),
+//            shippingTypes: Collection,
+//            designCategories: Collection,
+                designs: Collection.of(Design),
+                articleCategories: Collection.of(ArticleCategory),
+                articles: Collection.of(Article),
+                products: Collection.of(Product),
+//            applications: Collection,
+                currencies: Collection.of(Currency),
+//            languages: Collection,
+                countries: Collection.of(Country),
+                baskets: Collection.of(Basket)
+
+            }
+        });
+    });
+
--- sprd/model/Shop.js
+++ sprd/model/Shop.js
@@ -1,41 +1,8 @@
 define([
-    "sprd/data/SprdModel",
-    'js/data/Collection',
-    'sprd/model/Basket',
-    'sprd/model/Currency',
-    'sprd/model/Design',
-    'sprd/model/ProductType',
-    'sprd/model/ProductTypeDepartment',
-    'sprd/model/ArticleCategory',
-    'sprd/model/Article', 'sprd/model/User', 'sprd/model/Country', 'sprd/model/PrintType', 'sprd/model/FontFamily', "sprd/model/Product"],
-    function (SprdModel, Collection, Basket, Currency, Design, ProductType, ProductTypeDepartment, ArticleCategory, Article, User, Country, PrintType, FontFamily, Product) {
+    "sprd/model/AbstractShop"],
+    function (Shop) {
+        return Shop.inherit("sprd.model.Shop", {
 
-        return SprdModel.inherit("sprd.model.Shop", {
-            schema: {
-
-                user: User,
-                country: Country,
-//            language: SprdModel,
-                currency: Currency,
-//            address: SprdModel,
-
-                productTypes: Collection.of(ProductType),
-                printTypes: Collection.of(PrintType),
-                fontFamilies: Collection.of(FontFamily),
-                productTypeDepartments: Collection.of(ProductTypeDepartment),
-//            shippingTypes: Collection,
-//            designCategories: Collection,
-                designs: Collection.of(Design),
-            articleCategories: Collection.of(ArticleCategory),
-                articles: Collection.of(Article),
-            products: Collection.of(Product),
-//            applications: Collection,
-                currencies: Collection.of(Currency),
-//            languages: Collection,
-                countries: Collection.of(Country),
-                baskets: Collection.of(Basket)
-
-            }
         });
     });
 
