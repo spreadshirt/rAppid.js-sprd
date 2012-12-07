77d98b116647c4d6dcc1e9a441f5dba4d06831f4
Tony Findeisen <github@it-ony.de>
2012-12-07 16:41:19 +0100

    completely redesigned product viewer

--- sprd/view/ProductViewer.js
+++ sprd/view/ProductViewer.js
@@ -1,5 +1,5 @@
-define(['js/svg/Svg', 'js/svg/SvgElement', 'sprd/model/Product', 'underscore', 'sprd/data/ImageService', 'sprd/view/svg/ConfigurationViewer', 'sprd/view/svg/PrintAreaViewer'],
-    function(Svg, SvgElement, Product, _, ImageService, ConfigurationViewer, PrintAreaViewer){
+define(['js/svg/Svg', 'js/svg/SvgElement', 'sprd/model/Product', 'underscore', 'sprd/data/ImageService', 'sprd/view/svg/ConfigurationViewer', 'sprd/view/svg/PrintAreaViewer', 'sprd/view/svg/ProductTypeViewViewer'],
+    function(Svg, SvgElement, Product, _, ImageService, ConfigurationViewer, PrintAreaViewer, ProductTypeViewViewer){
 
     return Svg.inherit('sprd.view.ProductViewer',{
         defaults: {
@@ -7,24 +7,27 @@ define(['js/svg/Svg', 'js/svg/SvgElement', 'sprd/model/Product', 'underscore', '
             height: 300,
             width: 300,
 
-
+            // the view to show
             view: null,
 
+            // the product which is rendered
             product: null,
 
             // private defaults
             _productType: null,
-            _appearance: null
+            _appearance: null,
+            _view: null
         },
+
+        $classAttributes: ["product"],
 
         inject: {
             imageService: ImageService
         },
 
         ctor: function () {
-            this.$printAreas = {};
-            this.$renderedConfigurations = {};
-            this.$configurationViewer = {};
+
+            this.$productTypeViewViewerCache = {};
 
             this.callBase();
         },
@@ -32,238 +35,94 @@ define(['js/svg/Svg', 'js/svg/SvgElement', 'sprd/model/Product', 'underscore', '
         initialize: function(){
 
             this.bind('product','change:productType', this._onProductTypeChange, this);
-            this.bind('product','change:appearance', this._onAppearanceChange, this);
-
-            this.bind('product.configurations', 'add', this._onConfigurationAdded, this);
-            this.bind('product.configurations', 'remove', this._onConfigurationRemoved, this);
-//            this.bind('product.configurations', 'change:printArea', this._onConfigurationChange, this);
 
             this.callBase();
         },
 
-        _removeConfiguration: function(configuration) {
-
-            if (configuration) {
-                var configurationViewer = this.$configurationViewer[configuration.$cid];
-
-                if (configurationViewer) {
-                    configurationViewer.remove();
-                }
-
-                delete this.$configurationViewer[configuration.$cid];
-            }
-
-        },
-
-        _onConfigurationAdded: function (e) {
-
-            // create a new configuration viewer for the configuration
-            var configuration = e.$.item;
-            // add configuration viewer
-            this.$configurationViewer[configuration.$cid] = this.createComponent(ConfigurationViewer, {
-                configuration: configuration
-            });
-
-            this._renderConfiguration(configuration);
-
-        },
-
-        _onConfigurationRemoved: function (e) {
-            this._removeConfiguration(e.$);
-        },
-
-
-        _onConfigurationChange: function(e) {
-            this._renderConfiguration(e.$.item);
-        },
-
-        _onProductTypeChange: function(e){
-            // check if new productType has same appearance as set
-            var appearance = this.get('product.appearance');
-            if(e.$ && appearance){
-                appearance = e.$.getAppearanceById(appearance.$.id);
-            }
-            // if so, set this as appearance, else use null
-            this.set('_appearance', appearance, {silent: true});
-            this.set('_productType', e.$);
-        },
-        _onAppearanceChange: function(e){
-            this.set('_appearance', e.$);
-        },
-
-        _renderProduct: function(product, oldProduct) {
-            // product changed
-            if (this.$paper) {
-                this.$paper.clear();
-            }
-
-            if (oldProduct instanceof Object) {
-                // TODO: mkre, this is the first time string -> {product} -> should be null, because there isn't a previous attribute
-                for (var i = 0; i < oldProduct.get('configurations').length; i++) {
-                    this._removeConfiguration(oldProduct.get('configurations[' + i + ']'));
+        _onProductTypeChange: function(e) {
+            // remove all views
+            for (var key in this.$productTypeViewViewerCache) {
+                if (this.$productTypeViewViewerCache.hasOwnProperty(key)) {
+                    this.$productTypeViewViewerCache[key].remove();
+                    this.$productTypeViewViewerCache[key].destroy();
                 }
             }
 
-            this.set("_productType", this.get(product, "productType"));
+            this.$productTypeViewViewerCache = {};
+
+            var productType = e.$;
+
+            this.set('_productType', productType);
+
+            if (productType) {
+                this._determinateInternalView();
+            }
+
+        },
+
+        _commitView: function() {
+            this._determinateInternalView();
+        },
+
+        _determinateInternalView: function() {
+            var view = this.$.view,
+                productType = this.$._productType;
+
+            if (productType) {
+                if (view) {
+                    // check if view is compatible
+                    this.set('_view', productType.getViewById(view.$.id));
+                } else {
+                    this.set('_view', productType.getDefaultView());
+                }
+            }
+
         },
 
         _render_productType: function(productType) {
-            this._removeViews();
 
-            var view = this.$.view;
-            if(view){
-                view = productType.getViewByPerspective(view.$.perspective);
-                if(!view){
-                    view = productType.getDefaultView();
+            this._renderProductTypeView(productType, this.$._view);
+        },
+        
+        _render_view: function(view) {
+
+            view && this.setViewBox(0, 0, view.get('size.width'), view.get('size.height'));
+
+            this._renderProductTypeView(this.$._productType, view);
+        },
+
+        _renderProductTypeView: function(productType, view) {
+            if (productType && view && productType.containsView(view)) {
+                var cacheId = productType.$.id + "_" + view.$.id;
+
+                if (!this.$productTypeViewViewerCache[cacheId]) {
+                    this.$productTypeViewViewerCache[cacheId] = this.createComponent(ProductTypeViewViewer, {
+                        product: this.$.product,
+                        productViewer: this,
+
+                        _view: view,
+                        _productType: productType
+                    });
                 }
 
-                if(this.$.view === view){
-                    this._renderView(view);
-                }else{
-                    this.set('view', view);
-                }
-            }
-        },
-
-        _renderConfigurations: function() {
-
-
-            for (var key in this.$configurationViewer) {
-                if (this.$configurationViewer.hasOwnProperty(key)) {
-                    this.$configurationViewer.render(this.$paper);
-                }
-            }
-
-             var i;
-
-            for (i = 0; i < this.$renderedConfigurations.length; i++) {
-                this.$renderedConfigurations[i].remove();
-            }
-
-            this.$renderedConfigurations = [];
-
-            if (this.$.product) {
-                for (i = 0; i < this.$.product.$.configurations.size(); i++) {
-                    this._renderConfiguration(this.$.product.$.configurations.at(i));
-                }
-            }
-        },
-
-        _renderConfiguration: function(config) {
-            if (config) {
-                var configurationViewer = this.$configurationViewer[config.$cid];
-
-
-                if (this.$.view && this.$._productType) {
-
-                    var printAreaId = config.get('printArea.id'),
-                        printAreaViewer = this.$printAreas[printAreaId];
-
-                    if (printAreaViewer) {
-                        // print area rendered
-
-                        printAreaViewer.addChild(configurationViewer);
-
-                    }
-                } else {
-                    configurationViewer.remove();
-                }
-            }
-        },
-
-        _removeViews: function() {
-            if (this.$currentProductTypeView) {
-                this.$currentProductTypeView.remove();
-            }
-        },
-
-        _renderView: function(view) {
-            this._renderProductTypeViewAppearance();
-
-            // clear print areas
-            for (var key in this.$printAreas) {
-                if (this.$printAreas.hasOwnProperty(key)) {
-                    this.$printAreas[key].remove();
-                }
-            }
-
-            this.$printAreas = {};
-
-            var _productType = this.get('_productType');
-            if (view && _productType) {
-                // create print areas
-                for (var i = 0; i < view.$.viewMaps.length; i++) {
-                    var viewMap = view.$.viewMaps[i];
-                    var printArea = _productType.getPrintAreaById(viewMap.printArea.id);
-
-                    if (printArea && !this.$printAreas[printArea.id]) {
-
-                        // create a print area viewer
-                        var printAreaViewer = this.createComponent(PrintAreaViewer, {
-                            printArea: printArea,
-                            width: printArea.boundary.size.width,
-                            height: printArea.boundary.size.height
-                        }).translate(this.get(viewMap, 'offset.x'), this.get(viewMap, 'offset.y'));
-
-                        printAreaViewer.addTransform(this.get(viewMap, 'transformations.operations'));
-
-                        this.addChild(printAreaViewer);
-
-                        // and save
-                        this.$printAreas[printArea.id] = printAreaViewer;
-
-                    }
+                if (this.$currentProductTypeViewViewer) {
+                    this.$currentProductTypeViewViewer.remove();
                 }
 
-                this._renderConfigurations();
+
+                this.$currentProductTypeViewViewer = this.$productTypeViewViewerCache[cacheId];
+                this.addChild(this.$currentProductTypeViewViewer);
+
             }
-
-        },
-        _getTransformString: function(){
-
-        },
-        _render_appearance: function() {
-           this._renderProductTypeViewAppearance();
-        },
-
-        _renderProductTypeViewAppearance: function() {
-            this._removeViews();
-
-            var view = this.$.view,
-                appearance = this.$._appearance,
-                productType = this.$._productType;
-
-            if (view && appearance && productType) {
-                this.setViewBox(0, 0, view.get('size.width'), view.get('size.height'));
-
-                var url = this.$.imageService.productTypeImage(productType.$.id, view.$.id, appearance.$.id, {
-                    width: this.$.width,
-                    height: this.$.height
-                });
-
-                this.$currentProductTypeView = this.createComponent(SvgElement, {
-                    tagName: "image",
-                    href: url,
-                    x: 0,
-                    y: 0,
-                    width: view.get('size.width'),
-                    height: view.get('size.height')
-                });
-
-                this.addChild(this.$currentProductTypeView);
-                this.$currentProductTypeView.sendToBack();
-            }
-
-        },
-
-
-        destroy: function(){
-            this.unbind('product', 'change:productType', this._onProductTypeChange, this);
-            this.unbind('product', 'change:appearance', this._onAppearanceChange, this);
-            this.unbind('product.configurations', 'add', this._onConfigurationAdded, this);
-
-            this.callBase();
         }
+
+//        destroy: function(){
+//            this.unbind('product', 'change:productType', this._onProductTypeChange, this);
+//            this.unbind('product', 'change:appearance', this._onAppearanceChange, this);
+//            this.unbind('product.configurations', 'add', this._onConfigurationAdded, this);
+//
+//            this.callBase();
+//        }
 
     });
 
