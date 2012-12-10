7e75e64ee02bd6b2711dfbb426f8247657d83f9e
Tony Findeisen <github@it-ony.de>
2012-12-10 20:03:45 +0100

    modified printarea view

--- sprd/view/svg/PrintAreaViewer.js
+++ sprd/view/svg/PrintAreaViewer.js
@@ -1,27 +1,36 @@
 define(['js/svg/SvgElement'], function (SvgElement) {
 
-    var rRotationExtractor = /^rotate\(((?:[\-0-9]*\s?)*)\)$/;
-
     return SvgElement.inherit('sprd/view/svg/PrintAreaViewer', {
 
         defaults: {
             tagName: "g",
-            "class": "print-area"
+            componentClass: "print-area",
+
+            productTypeViewViewer: null,
+            product: null,
+
+            _viewMap: null
         },
 
-        $classAttributes: ["printArea"],
+        $classAttributes: ["product", "productTypeViewViewer"],
 
-        _initializeRenderer: function (el) {
+
+        _initializeRenderer: function () {
+
+            // Could be done via binding, but viewMaps don't change at runtime and so just evalulating
+            this.translate(this.get("_viewMap.offset.x"), this.get("_viewMap.offset.y"));
+            this.transform(this.get("_viewMap.transform"));
 
             var border = this.createComponent(SvgElement, {
                 tagName: "rect",
-                class: "print-area-border",
-                width: this.get('printArea.boundary.size.width'),
-                height: this.get('printArea.boundary.size.height')
+                componentClass: "print-area-border",
+                width: '{_viewMap.printArea.boundary.size.width}',
+                height: '{_viewMap.printArea.boundary.size.height}'
             });
 
             this.addChild(border);
 
+            this.callBase();
         },
 
         initialize: function () {
@@ -43,28 +52,6 @@ define(['js/svg/SvgElement'], function (SvgElement) {
 
         _onConfigurationChanged: function () {
             console.log("changed", arguments);
-        },
-
-        transform: function (matrix) {
-
-            if (matrix) {
-                var viewMap = this.$viewMap;
-
-                matrix.translate(viewMap.offset.x, viewMap.offset.y);
-
-                if (viewMap.transformations) {
-
-                    var matches = viewMap.transformations.operations.match(rRotationExtractor);
-                    if (matches && matches.length > 0) {
-                        var par = matches[1].split(" ");
-                        matrix.rotate(par[0], par[1], par[2]);
-                    }
-                }
-
-
-            }
-
-            return matrix;
         }
 
     });
