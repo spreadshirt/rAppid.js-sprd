afafc8c23e0c1b65b9eeaa7aca9fcb69f4198c68
Tony Findeisen <github@it-ony.de>
2012-12-10 12:56:58 +0100

    made getDefaultView also possible if default value isn't set

--- sprd/model/Product.js
+++ sprd/model/Product.js
@@ -6,8 +6,8 @@ define([
     'sprd/entity/DesignConfiguration',
     'sprd/entity/TextConfiguration',
     'sprd/entity/Appearance',
-    'js/data/TypeResolver', 'js/data/Entity'],
-    function (SprdModel, List, ProductType, AttributeTypeResolver, DesignConfiguration, TextConfiguration, Appearance, TypeResolver, Entity) {
+    'js/data/TypeResolver', 'js/data/Entity', "underscore", "flow"],
+    function (SprdModel, List, ProductType, AttributeTypeResolver, DesignConfiguration, TextConfiguration, Appearance, TypeResolver, Entity, _, flow) {
         return SprdModel.inherit("sprd.model.Product", {
 
             schema: {
@@ -24,6 +24,8 @@ define([
 
             defaults: {
                 productType: null,
+                appearance: null,
+                view: null,
                 configurations: List
             },
 
@@ -31,18 +33,28 @@ define([
                 // TODO format price with currency
                 return this.$.price.vatIncluded;
             },
+
             getDefaultView: function () {
-                if (this.$.defaultValues && this.$.productType) {
-                    return this.$.productType.getViewById(this.$.defaultValues.defaultView.id);
+
+                if (this.$.productType) {
+                    var defaultViewId = this.getDefaultViewId();
+
+                    if (defaultViewId !== null) {
+                        return this.$.productType.getViewById(defaultViewId);
+                    } else {
+                        return this.$.productType.getDefaultView();
+                    }
                 }
-                return null;
+
             },
+
             getDefaultViewId: function(){
                 if (this.$.defaultValues) {
                     return this.$.defaultValues.defaultView.id;
                 }
                 return null;
             },
+
             getDefaultAppearance: function() {
                 if(this.$.appearance && this.$.productType){
                     return this.$.productType.getAppearanceById(this.$.appearance.$.id);
