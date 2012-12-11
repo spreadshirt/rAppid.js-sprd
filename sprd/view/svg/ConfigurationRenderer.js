03906c45716cbc09a2c644a39dc6e09d0c34eff8
Tony Findeisen <github@it-ony.de>
2012-12-11 15:03:03 +0100

    set defaults for x, y, width, height for ConfigurationRenderer

--- sprd/view/svg/ConfigurationRenderer.js
+++ sprd/view/svg/ConfigurationRenderer.js
@@ -1,6 +1,13 @@
 define(['js/svg/SvgElement'], function(SvgElement) {
 
-    return SvgElement.inherit("sprd/view/svg/ConfigurationRenderer", {
+    return SvgElement.inherit("sprd.view.svg.ConfigurationRenderer", {
+        defaults: {
+            x: 0,
+            y: 0,
+            width: "{configuration.size().width}",
+            height: "{configuration.size().height}"
+        },
 
+        $classAttributes: ["configuration"]
     })
 });
