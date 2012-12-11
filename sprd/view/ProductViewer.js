b371ece14f5b4becc264b167792b53e4d91c5a2d
Tony Findeisen <github@it-ony.de>
2012-12-11 15:06:14 +0100

    formatted ProductViewer

--- sprd/view/ProductViewer.js
+++ sprd/view/ProductViewer.js
@@ -1,129 +1,129 @@
 define(['js/svg/Svg', 'js/svg/SvgElement', 'sprd/model/Product', 'underscore', 'sprd/data/ImageService', 'sprd/view/svg/ConfigurationViewer', 'sprd/view/svg/PrintAreaViewer', 'sprd/view/svg/ProductTypeViewViewer'],
-    function(Svg, SvgElement, Product, _, ImageService, ConfigurationViewer, PrintAreaViewer, ProductTypeViewViewer){
+    function (Svg, SvgElement, Product, _, ImageService, ConfigurationViewer, PrintAreaViewer, ProductTypeViewViewer) {
 
-    return Svg.inherit('sprd.view.ProductViewer',{
-        defaults: {
+        return Svg.inherit('sprd.view.ProductViewer', {
+            defaults: {
 
-            height: 300,
-            width: 300,
+                height: 300,
+                width: 300,
 
-            // the view to show
-            view: null,
+                // the view to show
+                view: null,
 
-            // the product which is rendered
-            product: null,
+                // the product which is rendered
+                product: null,
 
-            // private defaults
-            _productType: "{product.productType}",
-            _appearance: null,
-            _view: null,
+                // private defaults
+                _productType: "{product.productType}",
+                _appearance: null,
+                _view: null,
 
-            componentClass: "product-viewer"
-        },
+                componentClass: "product-viewer"
+            },
 
-        $classAttributes: ["product", "view"],
+            $classAttributes: ["product", "view"],
 
-        inject: {
-            imageService: ImageService
-        },
+            inject: {
+                imageService: ImageService
+            },
 
-        ctor: function () {
+            ctor: function () {
 
-            this.$productTypeViewViewerCache = {};
+                this.$productTypeViewViewerCache = {};
 
-            this.callBase();
-        },
+                this.callBase();
+            },
 
-        initialize: function(){
+            initialize: function () {
 
-            this.bind('product.productType', 'change:views', this._onViewsChanged, this);
+                this.bind('product.productType', 'change:views', this._onViewsChanged, this);
 
-            this.callBase();
-        },
+                this.callBase();
+            },
 
-        _removeProductTypeViewViewers: function() {
-            // remove all views
-            for (var key in this.$productTypeViewViewerCache) {
-                if (this.$productTypeViewViewerCache.hasOwnProperty(key)) {
-                    this.$productTypeViewViewerCache[key].remove();
-                    this.$productTypeViewViewerCache[key].destroy();
+            _removeProductTypeViewViewers: function () {
+                // remove all views
+                for (var key in this.$productTypeViewViewerCache) {
+                    if (this.$productTypeViewViewerCache.hasOwnProperty(key)) {
+                        this.$productTypeViewViewerCache[key].remove();
+                        this.$productTypeViewViewerCache[key].destroy();
+                    }
                 }
-            }
 
-            this.$productTypeViewViewerCache = {};
-        },
+                this.$productTypeViewViewerCache = {};
+            },
 
-        _onViewsChanged: function() {
-            this._determinateInternalView();
-        },
-
-        _commitView: function() {
-            this._determinateInternalView();
-        },
-
-        _determinateInternalView: function() {
-            var view = this.$.view,
-                productType = this.$._productType;
-
-            if (productType) {
-                if (view) {
-                    // check if view is compatible
-                    this.set('_view', productType.getViewById(view.$.id));
-                } else {
-                    this.set('_view', productType.getDefaultView());
-                }
-            }
-
-        },
-
-        _render_productType: function(productType, oldProductType) {
-
-            oldProductType && this._removeProductTypeViewViewers();
-
-            if (productType) {
+            _onViewsChanged: function () {
                 this._determinateInternalView();
-                this._renderProductTypeView(productType, this.$._view);
-            }
+            },
 
-        },
-        
-        _render_view: function(view) {
+            _commitView: function () {
+                this._determinateInternalView();
+            },
 
-            view && this.setViewBox(0, 0, view.get('size.width'), view.get('size.height'));
+            _determinateInternalView: function () {
+                var view = this.$.view,
+                    productType = this.$._productType;
 
-            this._renderProductTypeView(this.$._productType, view);
-        },
-
-        _renderProductTypeView: function(productType, view) {
-            if (productType && view && productType.containsView(view)) {
-                var cacheId = productType.$.id + "_" + view.$.id;
-
-                if (!this.$productTypeViewViewerCache[cacheId]) {
-                    this.$productTypeViewViewerCache[cacheId] = this.createComponent(ProductTypeViewViewer, {
-                        product: this.$.product,
-                        productViewer: this,
-
-                        _view: view,
-                        _productType: productType
-                    });
+                if (productType) {
+                    if (view) {
+                        // check if view is compatible
+                        this.set('_view', productType.getViewById(view.$.id));
+                    } else {
+                        this.set('_view', productType.getDefaultView());
+                    }
                 }
 
-                if (this.$currentProductTypeViewViewer) {
-                    this.$currentProductTypeViewViewer.remove();
+            },
+
+            _render_productType: function (productType, oldProductType) {
+
+                oldProductType && this._removeProductTypeViewViewers();
+
+                if (productType) {
+                    this._determinateInternalView();
+                    this._renderProductTypeView(productType, this.$._view);
                 }
 
-                this.$currentProductTypeViewViewer = this.$productTypeViewViewerCache[cacheId];
-                this.addChild(this.$currentProductTypeViewViewer);
+            },
 
+            _render_view: function (view) {
+
+                view && this.setViewBox(0, 0, view.get('size.width'), view.get('size.height'));
+
+                this._renderProductTypeView(this.$._productType, view);
+            },
+
+            _renderProductTypeView: function (productType, view) {
+                if (productType && view && productType.containsView(view)) {
+                    var cacheId = productType.$.id + "_" + view.$.id;
+
+                    if (!this.$productTypeViewViewerCache[cacheId]) {
+                        this.$productTypeViewViewerCache[cacheId] = this.createComponent(ProductTypeViewViewer, {
+                            product: this.$.product,
+                            productViewer: this,
+
+                            _view: view,
+                            _productType: productType
+                        });
+                    }
+
+                    if (this.$currentProductTypeViewViewer) {
+                        this.$currentProductTypeViewViewer.remove();
+                    }
+
+                    this.$currentProductTypeViewViewer = this.$productTypeViewViewerCache[cacheId];
+                    this.addChild(this.$currentProductTypeViewViewer);
+
+                }
+            },
+
+            destroy: function () {
+                this._removeProductTypeViewViewers();
+                this.callBase();
             }
-        },
 
-        destroy: function(){
-            this._removeProductTypeViewViewers();
-            this.callBase();
-        }
+        });
+
 
     });
-
-
-});
