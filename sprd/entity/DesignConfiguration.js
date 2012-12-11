cfa9d0c14ba63dda080810d39314face6e264a46
Tony Findeisen <github@it-ony.de>
2012-12-11 15:00:55 +0100

    implemented size() method for DesignConfiguration

--- sprd/entity/DesignConfiguration.js
+++ sprd/entity/DesignConfiguration.js
@@ -1,4 +1,12 @@
-define(['sprd/entity/Configuration'], function (Configuration) {
+define(['sprd/entity/Configuration', 'sprd/entity/Size'], function (Configuration, Size) {
 	return Configuration.inherit('sprd.model.DesignConfiguration', {
+
+        size: function() {
+            if (this.$.design) {
+                return this.$.design.$.size;
+            }
+
+            return Size.empty;
+        }
 	});
 });
