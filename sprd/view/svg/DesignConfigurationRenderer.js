d1055af2d610aa91daee55eeaf6fd384e786438e
Tony Findeisen <github@it-ony.de>
2012-12-11 15:04:05 +0100

    implemented DesignConfigurationRenderer

--- sprd/view/svg/DesignConfigurationRenderer.js
+++ sprd/view/svg/DesignConfigurationRenderer.js
@@ -1,6 +1,26 @@
-define(['sprd/view/svg/ConfigurationRenderer'], function (Renderer) {
+define(['sprd/view/svg/ConfigurationRenderer', "sprd/data/ImageService"], function (Renderer, ImageService) {
 
-    return Renderer.inherit("sprd/view/svg/TextConfigurationRenderer", {
+    return Renderer.inherit("sprd.view.svg.DesignConfigurationRenderer", {
+
+        defaults: {
+            tagName: "image",
+            href: "{url()}"
+        },
+
+        inject: {
+            imageService: ImageService
+        },
+
+        url: function () {
+            if (this.$.imageService && this.$.configuration && this.$.configuration.$.design) {
+                return this.$.imageService.designImage(this.$.configuration.$.design.$.id, {
+                    width: this.$._width,
+                    height: this.$._height
+                });
+            }
+
+            return null;
+        }.onChange("design")
 
     })
 });
