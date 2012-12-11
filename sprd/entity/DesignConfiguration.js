d39e59bdf2f741ccfa13a27ecebbe8e77bd5bee0
Tony Findeisen <github@it-ony.de>
2012-12-11 19:07:45 +0100

    size calculation for DesignConfiguration depending on the print type

--- sprd/entity/DesignConfiguration.js
+++ sprd/entity/DesignConfiguration.js
@@ -1,12 +1,28 @@
-define(['sprd/entity/Configuration', 'sprd/entity/Size'], function (Configuration, Size) {
+define(['sprd/entity/Configuration', 'sprd/entity/Size', 'sprd/util/UnitUtil'], function (Configuration, Size, UnitUtil) {
 	return Configuration.inherit('sprd.model.DesignConfiguration', {
 
+        defaults: {
+            _dpi: "{printType.dpi}"
+        },
+
+        ctor: function() {
+            this.$sizeCache = {};
+
+            this.callBase();
+        },
+
         size: function() {
-            if (this.$.design) {
-                return this.$.design.$.size;
+
+            if (this.$.design && this.$.printType && this.$.printType.$.dpi) {
+                var dpi = this.$.printType.$.dpi;
+                if (!this.$sizeCache[dpi]) {
+                    this.$sizeCache[dpi] = UnitUtil.convertSizeToMm(this.$.design.$.size, dpi);
+                }
+
+                return  this.$sizeCache[dpi];
             }
 
             return Size.empty;
-        }
+        }.onChange("_dpi", "design")
 	});
 });
