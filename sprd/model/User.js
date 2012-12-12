4006d3fe4654c31f3f5ec1530c3fd9df08a89e9b
Tony Findeisen <github@it-ony.de>
2012-12-12 14:34:30 +0100

    imports for user

--- sprd/model/User.js
+++ sprd/model/User.js
@@ -1,10 +1,10 @@
-define(['sprd/data/SprdModel', 'js/data/Collection', 'sprd/model/UserShop', "sprd/model/UserDesign","sprd/model/UserProductType", "sprd/model/UserProduct"], function (SprdModel, Collection, UserShop, UserDesign, UserProductType, UserProduct) {
+define(['sprd/data/SprdModel', 'js/data/Collection', 'sprd/model/Shop', "sprd/model/Design","sprd/model/ProductType", "sprd/model/Product"], function (SprdModel, Collection, Shop, Design, ProductType, Product) {
 	return SprdModel.inherit('sprd.model.User', {
         schema: {
-            shops: Collection.of(UserShop),
-            designs: Collection.of(UserDesign),
-            products: Collection.of(UserProduct),
-            productTypes: Collection.of(UserProductType)
+            shops: Collection.of(Shop),
+            designs: Collection.of(Design),
+            products: Collection.of(Product),
+            productTypes: Collection.of(ProductType)
         }
 	});
 });
