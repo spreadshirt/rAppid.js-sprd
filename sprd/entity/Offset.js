1bc6b85613de040fba79dbfc4d32940797265c0c
Tony Findeisen <github@it-ony.de>
2012-12-13 18:46:54 +0100

    added schema for offset

--- sprd/entity/Offset.js
+++ sprd/entity/Offset.js
@@ -2,7 +2,14 @@ define(['js/data/Entity'], function(Entity) {
     return Entity.inherit('sprd.entity.Offset', {
         defaults: {
             x: 0,
-            y: 0
+            y: 0,
+            unit: "mm"
+        },
+
+        schema: {
+            x: Number,
+            y: Number,
+            unit: String
         }
     })
 });
