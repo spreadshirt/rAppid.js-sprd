43ec51e65e23d74aaebf7582c94086842a96079b
Marcus Krejpowicz <krebbl@gmail.com>
2012-12-14 09:16:43 +0100

    removed href stuff

--- sprd/model/processor/DefaultProcessor.js
+++ sprd/model/processor/DefaultProcessor.js
@@ -4,23 +4,8 @@ define(['js/data/DataSource'], function (DataSource) {
         _composeSubModel: function (model, action, options) {
             // we do not want to compose sub models, but rather return a reference to the model
             return {
-                id: model.$.id,
-                // TODO: determinate href
-                href: model.$.href
+                id: model.$.id
             }
-        },
-
-        _getIdForValue: function (value) {
-
-            if (value.id && value.href) {
-                var id = value.href.split("/").pop();
-                if (id) {
-                    value.id = id;
-                    return id;
-                }
-            }
-
-            return value.id;
         }
 
     });
