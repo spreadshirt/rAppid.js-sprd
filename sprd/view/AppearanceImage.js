e6951172e7b4bc6bb50d545c21bb0300efdd440c
Marcus Krejpowicz <krebbl@gmail.com>
2012-12-17 10:34:39 +0100

    fixed color selector and added appearance image

---
+++ sprd/view/AppearanceImage.js
@@ -0,0 +1,38 @@
+define(["xaml!sprd/view/Image", "sprd/data/ImageService"], function (Image, ImageService) {
+
+
+    return Image.inherit('sprd.view.AppearanceImage', {
+
+        defaults: {
+            // if null use default view
+            view: null,
+            appearance: null
+        },
+
+        inject: {
+            imageService: ImageService
+        },
+
+        _commitChangedAttributes: function (attributes) {
+            this.callBase();
+            if (attributes.hasOwnProperty('appearance')) {
+                this.set('loaded', false);
+            }
+        },
+
+        imageUrl: function () {
+            var url = null,
+                imageService = this.$.imageService,
+                appearance = this.$.appearance;
+
+            if (appearance && imageService) {
+                return imageService.appearanceImage(this.$.appearance.$.id, {
+                    width: this.$.width,
+                    height: this.$.height
+                });
+            }
+            return url;
+
+        }.onChange('design')
+    });
+});
