f432afaf7aaadbb6ed4b27b987676335a1ffbb81
Marcus Krejpowicz <krebbl@gmail.com>
2012-12-17 13:41:10 +0100

    fixed code style

--- sprd/entity/ConcreteElement.js
+++ sprd/entity/ConcreteElement.js
@@ -2,7 +2,7 @@ define(["js/data/Entity", "sprd/model/Product", "sprd/model/Article", "sprd/enti
     var TYPE_PRODUCT = "sprd:product";
     var TYPE_ARTICLE = "sprd:article";
 
-    return Entity.inherit("sprd.entity.ConcreteElement",{
+    return Entity.inherit("sprd.entity.ConcreteElement", {
         schema: {
             appearance: Appearance,
             size: Entity
@@ -14,29 +14,29 @@ define(["js/data/Entity", "sprd/model/Product", "sprd/model/Article", "sprd/enti
             item: null
         },
 
-        isEqual: function(concreteElement){
+        isEqual: function (concreteElement) {
             return this.get('appearance.id') === concreteElement.get('appearance.id') &&
-                this.get('size.id')  === concreteElement.get('size.id') &&
+                this.get('size.id') === concreteElement.get('size.id') &&
                 this.get('item.id') === concreteElement.get('item.id');
         },
 
-        getProduct: function(){
-            if(this.$.item instanceof Product){
+        getProduct: function () {
+            if (this.$.item instanceof Product) {
                 return this.$.item;
-            }else if(this.$.item instanceof Article){
+            } else if (this.$.item instanceof Article) {
                 return this.$.item.$.product;
             }
             return null;
-        }.on(['item','change:product']),
+        }.on(['item', 'change:product']),
 
-        getArticle: function() {
+        getArticle: function () {
             if (this.$.item instanceof Article) {
                 return this.$.item;
             }
             return null;
         },
 
-        getType: function(){
+        getType: function () {
             if (this.$.item instanceof Product) {
                 return TYPE_PRODUCT;
             } else if (this.$.item instanceof Article) {
