ea03636787a017de4f2410b8a7be0c2d7a88bcfa
Marcus Krejpowicz <krebbl@gmail.com>
2012-12-17 13:40:26 +0100

    fixed code style

--- sprd/entity/Appearance.js
+++ sprd/entity/Appearance.js
@@ -1,4 +1,4 @@
-define(['js/data/Entity', 'sprd/model/PrintType', 'sprd/entity/AppearanceColor'], function(Entity, PrintType, AppearanceColor) {
+define(['js/data/Entity', 'sprd/model/PrintType', 'sprd/entity/AppearanceColor'], function (Entity, PrintType, AppearanceColor) {
     return Entity.inherit('sprd.entity.Appearance', {
         schema: {
             name: String,
@@ -6,7 +6,7 @@ define(['js/data/Entity', 'sprd/model/PrintType', 'sprd/entity/AppearanceColor']
             colors: [AppearanceColor]
         },
 
-        getMainColor: function() {
+        getMainColor: function () {
             return this.get("colors[0].value");
         }
     })
