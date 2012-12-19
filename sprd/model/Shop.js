86dd99b0a7366014d9b643fbc7d69df88233da2b
Tony Findeisen <github@it-ony.de>
2012-12-19 18:21:48 +0100

    added isMarketPlace()

--- sprd/model/Shop.js
+++ sprd/model/Shop.js
@@ -1,5 +1,15 @@
 define(["sprd/model/AbstractShop"], function (Shop) {
     return Shop.inherit("sprd.model.Shop", {
+
+        defaults: {
+            platform: null
+        },
+
+        isMarketPlace: function() {
+            var platform = this.$.platform,
+                id = this.$.id;
+            return (platform === "EU" && id == 205909) || (platform === "NA" && id == 93439);
+        }.onChange("platform", "id")
     });
 });
 
