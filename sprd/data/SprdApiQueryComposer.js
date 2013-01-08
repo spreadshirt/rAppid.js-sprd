7361a5cec55658306e22d0789526d15747c25f41
Marcus Krejpowicz <krebbl@gmail.com>
2013-01-08 19:14:58 +0100

    fixed Composer

--- sprd/data/SprdApiQueryComposer.js
+++ sprd/data/SprdApiQueryComposer.js
@@ -1,35 +1,43 @@
-define(["js/data/Query"], function(Query){
+define(["js/data/Query"], function (Query) {
 
 
     SprdApiQueryComposer = {
 
-        compose: function(query){
+        compose: function (query) {
 
             query = query.query;
             var params = {};
 
-            if(query.sort){
+            if (query.sort) {
                 params.sortField = query.sort[0].field;
                 params.sortOrder = query.sort[0].direction ? "asc" : "desc";
             }
 
-            if(query.where){
+            if (query.where) {
                 params.query = this.translateOperator(query.where);
+            }
+
+            if (query.extra) {
+                for (var key in query.extra) {
+                    if (query.extra.hasOwnProperty(key)) {
+                        params[key] = query.extra[key];
+                    }
+                }
             }
 
             return params;
         },
 
-        translateOperator: function(operator, depth){
+        translateOperator: function (operator, depth) {
             depth = depth === undefined ? 0 : depth;
             var name = operator.operator;
-            if(operator instanceof Query.Where){
-                return this.translateExpressions(operator.expressions, depth +1).join(" ");
-            } else if(operator instanceof Query.Comparator){
+            if (operator instanceof Query.Where) {
+                return this.translateExpressions(operator.expressions, depth + 1).join(" ");
+            } else if (operator instanceof Query.Comparator) {
 
-                if(name === "in"){
-                    return ["+",operator.field,"(",operator.value.join(","),")"].join("");
-                } else if (name === "eql"){
+                if (name === "in") {
+                    return ["+",operator.field,":(", operator.value.join(" "),")"].join("");
+                } else if (name === "eql") {
                     return operator.value;
                 }
             }
