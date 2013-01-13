d3fccf975fadf0ffbd8a9adf38748fd7a1bb8604
Tony Findeisen <github@it-ony.de>
2013-01-13 10:37:31 +0100

    added file to schema of UploadImage

--- sprd/model/UploadImage.js
+++ sprd/model/UploadImage.js
@@ -2,6 +2,10 @@ define(['sprd/data/SprdModel'], function(SprdModel) {
     return SprdModel.inherit('sprd.model.UploadImage', {
         defaults: {
             file: null
+        },
+
+        schema: {
+            file: Object
         }
     });
 });
