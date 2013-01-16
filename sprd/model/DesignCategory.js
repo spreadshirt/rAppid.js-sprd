748569c7b99cfe5c60775fa815b19d225204b93c
Marcus Krejpowicz <krebbl@gmail.com>
2013-01-16 17:57:58 +0100

    added DesignCategory

---
+++ sprd/model/DesignCategory.js
@@ -0,0 +1,29 @@
+define(["sprd/data/SprdModel", "sprd/model/Design", "js/data/Collection"], function (Model, Design, Collection) {
+
+    var DesignCategory = Model.inherit('sprd.model.DesignCategory', {
+        isMarketPlace: function(){
+            return this.$.id === "b1000000";
+        },
+        getSubCategories: function () {
+            return this.$.designCategories;
+        },
+        getSubCategoryById: function (id) {
+            return this.$.designCategories.each(function (val) {
+                if (val.$.id == id) {
+                    this['return'](val);
+                }
+            });
+        },
+        parse: function(data){
+            data.id = "b" + data.id;
+            return data;
+        }
+    });
+
+    DesignCategory.prototype.schema = {
+        designCategories: [DesignCategory],
+        designs: Collection.of(Design)
+    };
+
+    return DesignCategory;
+});
