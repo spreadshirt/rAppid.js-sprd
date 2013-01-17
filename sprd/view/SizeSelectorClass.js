f0e0e81afe46bfcf9c1c3ecf3012f4067517ed63
Marcus Krejpowicz <krebbl@gmail.com>
2013-01-17 15:39:41 +0100

    fixed simple size selector

--- sprd/view/SizeSelectorClass.js
+++ sprd/view/SizeSelectorClass.js
@@ -1,32 +1,34 @@
-define(['js/ui/SelectionView'], function(SelectionView) {
+define(['js/ui/SelectionView'], function (SelectionView) {
     return SelectionView.inherit({
         defaults: {
             componentClass: 'product-sizes',
             itemKey: 'size',
             // if null use default view
-            sizes: null,
+            productType: null,
+            appearance: null,
+            items: "{productType.sizes}",
             multiSelect: false
         },
-
-        _commitChangedAttributes: function (attributes) {
-            if (attributes.sizes) {
-                // if the article has the same
-                if (attributes.sizes !== this.$items) {
-                    this.set('items', attributes.sizes);
-                    if (this.$.items.length === 1) {
-                        this.set('selectedItem', this.$.items[0]);
-                    }
-                }
-            } else {
-                // set items to null
-                // this.set({needsSelection: false, selectedItem: null});
-                this.set('items', []);
-            }
-
+        _commitChangedAttributes: function(attributes){
             this.callBase();
+            if (this._hasSome(attributes, ["appearance"])) {
+                var self = this;
+                if (this.$lastSelectedSize) {
+                    this.$.productType.getAvailableSizesForAppearance(this.$.appearance).each(function (size) {
+                        if (size.$.name == self.$lastSelectedSize.$.name || size.$.id == self.$lastSelectedSize.$.id) {
+                            self.set('selectedItem', size);
+                        }
+                    });
+                }
+            }
         },
-        selectSize: function (e, element) {
-            this.set('selectedItem', element.find('$size'));
+        _commitSelectedItem: function(item){
+            if(item){
+                this.$lastSelectedSize = item;
+            }
+        },
+        availableClass: function (size) {
+            return this.$.productType.isSizeAndAppearanceAvailable(size, this.$.appearance) ? "available" : "not-available";
         }
     })
 });
