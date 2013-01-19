61fd1891ff9e05c46bedc7324707d46e091cdc9e
Tony Findeisen <github@it-ony.de>
2013-01-19 18:22:22 +0100

    added hard boundary check based on configuration bounding box

--- sprd/entity/Configuration.js
+++ sprd/entity/Configuration.js
@@ -1,4 +1,4 @@
-define(['js/data/Entity', 'sprd/entity/Offset', 'sprd/entity/Size', 'sprd/entity/PrintArea', 'sprd/model/PrintType', 'js/core/List' , "sprd/entity/Price"], function (Entity, Offset, Size, PrintArea, PrintType, List, Price) {
+define(['js/data/Entity', 'sprd/entity/Offset', 'sprd/entity/Size', 'sprd/entity/PrintArea', 'sprd/model/PrintType', 'js/core/List' , "sprd/entity/Price", "sprd/type/Matrix2d"], function (Entity, Offset, Size, PrintArea, PrintType, List, Price, Matrix2d) {
 
     var undefined;
 
@@ -42,12 +42,13 @@ define(['js/data/Entity', 'sprd/entity/Offset', 'sprd/entity/Size', 'sprd/entity
                 sizeChanged = this._hasSome($, ["_size", "_x", "_y", "scale", "offset"]),
                 printTypeChanged = this._hasSome($, ["printType"]),
                 width, height,
-                scale = $.scale || this.$.scale;
+                scale = $.scale || this.$.scale,
+                rotation = $.rotation || this.$.rotation;
 
             if (sizeChanged || rotationChanged) {
                 width = this.width(scale.x);
                 height = this.height(scale.y);
-                this._setError("hardBoundary", this._hasHardBoundaryError($.offset || this.$.offset, width, height));
+                this._setError("hardBoundary", this._hasHardBoundaryError($.offset || this.$.offset, width, height, rotation));
             }
 
             if (sizeChanged || printTypeChanged) {
@@ -71,7 +72,7 @@ define(['js/data/Entity', 'sprd/entity/Offset', 'sprd/entity/Size', 'sprd/entity
             });
         },
 
-        _hasHardBoundaryError: function (offset, width, height) {
+        _hasHardBoundaryError: function (offset, width, height, rotation) {
 
             var printArea = this.$.printArea;
 
@@ -79,12 +80,66 @@ define(['js/data/Entity', 'sprd/entity/Offset', 'sprd/entity/Size', 'sprd/entity
                 return;
             }
 
+
+            var boundingBox = this._getBoundingBox(offset,width,height, rotation);
+            console.log(boundingBox);
+
+            return !(boundingBox.x >= 0 && boundingBox.y >= 0 &&
+                (boundingBox.x + boundingBox.width) <= printArea.get("boundary.size.width") &&
+                (boundingBox.y + boundingBox.height) <= printArea.get("boundary.size.height"));
+
+        },
+
+        _getBoundingBox: function(offset, width, height, rotation) {
+
             var x = offset.$.x,
                 y = offset.$.y;
 
-            return !(x >= 0 && y >= 0 &&
-                (x + width) <= printArea.get("boundary.size.width") &&
-                (y + height) <= printArea.get("boundary.size.height"));
+            if (!rotation) {
+                return {
+                    x: x,
+                    y: y,
+                    height: height,
+                    width: width
+                };
+            }
+
+            var minX,
+                maxX,
+                minY,
+                maxY,
+                matrix = new Matrix2d().rotateDeg(rotation);
+
+            var halfW = width / 2,
+                halfH = height /2;
+
+            var points = [
+                [-halfW, -halfH],
+                [halfW, -halfH],
+                [halfW, halfH],
+                [-halfW, halfH]
+            ];
+
+            var point = matrix.transformPoint(points[0]);
+            minX = maxX = point[0];
+            minY = maxY = point[1];
+
+            for (var i = 1; i < points.length; i++) {
+                point = matrix.transformPoint(points[i]);
+
+                minX = Math.min(point[0], minX);
+                maxX = Math.max(point[0], maxX);
+                minY = Math.min(point[1], minY);
+                maxY = Math.max(point[1], maxY);
+            }
+
+            return {
+                x: minX + halfW + x,
+                y: minY + halfH + y,
+                width: maxX - minX,
+                height: maxY - minY
+            };
+
         },
 
         size: function () {
