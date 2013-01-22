ba0879f99449d03d693696273cd8e42583eee256
Tony Findeisen <github@it-ony.de>
2013-01-22 12:41:44 +0100

    added BasketService

---
+++ sprd/data/BasketService.js
@@ -0,0 +1,11 @@
+define(["js/core/Base"], function(Base) {
+
+    return Base.inherit('sprd.data.BasketService', {
+
+        addBasketItem: function (element, quantity, callback) {
+
+        }
+
+    });
+
+});
