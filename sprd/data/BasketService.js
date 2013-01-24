3959fde9c233b345ddf8922a146c828ccdfab00d
Tony Findeisen <github@it-ony.de>
2013-01-24 13:06:49 +0100

    invoke callback on basketservice

--- sprd/data/BasketService.js
+++ sprd/data/BasketService.js
@@ -3,7 +3,7 @@ define(["js/core/Base"], function(Base) {
     return Base.inherit('sprd.data.BasketService', {
 
         addBasketItem: function (element, quantity, callback) {
-
+            callback && callback();
         }
 
     });
