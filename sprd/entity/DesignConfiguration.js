b0579a8eb93cfe332783ba065391b58e0273cf2a
Marcus Krejpowicz <krebbl@gmail.com>
2013-01-28 10:00:20 +0100

    fixed composing of printColors

--- sprd/entity/DesignConfiguration.js
+++ sprd/entity/DesignConfiguration.js
@@ -221,6 +221,8 @@ define(['sprd/entity/Configuration', 'sprd/entity/Size', 'sprd/util/UnitUtil', '
                 ret.content.svg.image.printColorRGBs = printColorRGBs.join(" ");
             }
 
+            ret.printColors = undefined;
+
             ret.restrictions = {
                 changeable: true
             };
