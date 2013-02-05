c9e980205a0f9f8b7d86eb9a9d28e3bfa117ef77
Tony Findeisen <github@it-ony.de>
2013-02-05 16:50:19 +0100

    extended schema for FontFamily

--- sprd/model/FontFamily.js
+++ sprd/model/FontFamily.js
@@ -1,4 +1,11 @@
-define(["sprd/data/SprdModel"], function (SprdModel) {
+define(["sprd/data/SprdModel", "js/data/Collection", "sprd/model/PrintType", "sprd/entity/Font"], function (SprdModel, Collection, PrintType, Font) {
     return SprdModel.inherit("sprd.model.FontFamily", {
+
+        schema: {
+            printTypes: Collection.of(PrintType),
+            fonts: [Font],
+            deprecated: Boolean
+        }
+
     })
 });
