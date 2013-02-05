37557020903d6dbb117238867c875f736b1f1dea
Tony Findeisen <github@it-ony.de>
2013-02-05 18:09:11 +0100

    added helper methods to FontFamily and fixed bug in schema

--- sprd/model/FontFamily.js
+++ sprd/model/FontFamily.js
@@ -1,10 +1,34 @@
-define(["sprd/data/SprdModel", "js/data/Collection", "sprd/model/PrintType", "sprd/entity/Font"], function (SprdModel, Collection, PrintType, Font) {
+define(["sprd/data/SprdModel", "sprd/model/PrintType", "sprd/entity/Font"], function (SprdModel, PrintType, Font) {
     return SprdModel.inherit("sprd.model.FontFamily", {
 
         schema: {
-            printTypes: Collection.of(PrintType),
+            printTypes: [PrintType],
             fonts: [Font],
             deprecated: Boolean
+        },
+
+        getFont: function (weight, style) {
+            weight = weight || "normal";
+            style = style || "normal";
+
+            for (var i = 0; i < this.$.fonts.$items.length; i++) {
+                var font = this.$.fonts.$items[i];
+                if (font.$.weight === weight || font.$.style === style) {
+                    return font;
+                }
+            }
+
+            return null;
+
+        },
+
+        getDefaultFont: function () {
+
+            if (!this.$.fonts) {
+                return null;
+            }
+
+            return this.$.fonts.at(0);
         }
 
     })
