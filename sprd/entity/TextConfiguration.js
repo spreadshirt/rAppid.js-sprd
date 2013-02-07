eea464afde9c37109cfeabc8f48baf5f04bc6f71
Tony Findeisen <github@it-ony.de>
2013-02-07 14:23:00 +0100

    added init method for TextConfiguration

--- sprd/entity/TextConfiguration.js
+++ sprd/entity/TextConfiguration.js
@@ -1,14 +1,49 @@
-define(['sprd/entity/Configuration'], function (Configuration) {
+define(['sprd/entity/Configuration', "flow", 'sprd/entity/Size'], function (Configuration, flow, Size) {
 	return Configuration.inherit('sprd.entity.TextConfiguration', {
 		defaults : {
             text: "",
             font: null,
             fontSize: 48,
-            typeFace: "regular"
+            typeFace: "regular",
+
+            textArea: null
         },
 
         init: function(callback) {
-            callback && callback("Textconfigurations aren't supported yet");
-        }
+
+            var self = this,
+                $$ = self.$$,
+//                svg = $$.svg,
+                printType = this.$.printType,
+                printArea;
+
+            flow()
+                .seq(function(cb) {
+                    printType.fetch(null, cb);
+                })
+                .seq(function() {
+                    if ($$ && $$.printArea) {
+                        printArea = self.$context.$contextModel.$.productType.getPrintAreaById($$.printArea.$.id)
+                    } else {
+                        printArea = self.$.printArea;
+                    }
+                })
+                .seq(function() {
+                    if (!self.textArea) {
+
+                        var size = printArea.get("defaultBox") || printArea.get("boundary.size");
+
+                        self.set("textArea", new Size({
+                            width: self.get(size, "width"),
+                            height: self.get(size, "height")
+                        }));
+                    }
+                })
+                .exec(callback);
+        },
+
+        size: function() {
+            return this.$.textArea || Size.empty;
+        }.onChange("textArea")
 	});
 });
