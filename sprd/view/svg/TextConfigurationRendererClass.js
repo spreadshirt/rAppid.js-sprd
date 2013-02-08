97e7d5c10dd5f6c2e849e5767aa786349a2540eb
Tony Findeisen <github@it-ony.de>
2013-02-08 15:36:13 +0100

    extended TextConfigurationRenderer

--- sprd/view/svg/TextConfigurationRendererClass.js
+++ sprd/view/svg/TextConfigurationRendererClass.js
@@ -1,4 +1,4 @@
-define(['sprd/view/svg/ConfigurationRenderer'], function(ConfigurationRenderer) {
+define(['sprd/view/svg/ConfigurationRenderer', 'underscore'], function(ConfigurationRenderer, _) {
 
     return ConfigurationRenderer.inherit("sprd.view.svg.TextConfigurationRendererClass", {
 
@@ -7,8 +7,43 @@ define(['sprd/view/svg/ConfigurationRenderer'], function(ConfigurationRenderer) 
             componentClass: "text-configuration"
         },
 
-        ctor: function() {
+        _initialize: function() {
+
             this.callBase();
+
+            var fonts = this.getFonts(),
+                svgRoot = this.getSvgRoot();
+
+            for (var i = 0; i < fonts.length; i++) {
+                var font = fonts[i];
+                svgRoot.fontManager.loadExternalFont(font.getUniqueFontName(), "fonts/svg/" + font.$.id + ".svg#font");
+            }
+        },
+
+        getFonts: function() {
+            var fonts = [];
+
+            var configuration = this.$.configuration;
+            if (configuration && configuration.$.textFlow) {
+                addFonts(configuration.$.textFlow);
+            }
+
+            return fonts;
+
+            function addFonts(flowElement) {
+                if (flowElement) {
+                    var font = flowElement.$.style.$.font;
+
+                    if (font && _.indexOf(fonts, font) === -1) {
+                        fonts.push(font);
+                    }
+
+                    !flowElement.isLeaf && flowElement.$.children.each(function(child) {
+                        addFonts(child);
+                    });
+                }
+            }
         }
+
     });
 });
