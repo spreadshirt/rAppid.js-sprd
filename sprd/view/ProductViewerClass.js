10d7abffc74b8de9df664e8f545806af81dfeb7e
Tony Findeisen <github@it-ony.de>
2013-02-11 15:33:49 +0100

    remove tabIndex for productViewer by default

--- sprd/view/ProductViewerClass.js
+++ sprd/view/ProductViewerClass.js
@@ -1,4 +1,4 @@
-define(["js/ui/View"], function(View) {
+define(["js/ui/View"], function (View) {
     return View.inherit('sprd.view.ProductViewerClass', {
 
         defaults: {
@@ -7,18 +7,73 @@ define(["js/ui/View"], function(View) {
             width: 300,
             height: 300,
             selectedConfiguration: null,
-            editable: true,
-            tabindex: 1
+            editable: true
         },
 
         _clickHandler: function (e) {
-            if (this.$.editable && !e.isDefaultPrevented) {
+            if (this.$.editable && !(e.isDefaultPrevented || e.defaultPrevented)) {
                 this.set('selectedConfiguration', null);
             }
 
-            if (this.$el && this.$el.focus) {
-                this.$el.focus();
+            e.stopPropagation();
+
+        },
+
+        _keyDownHandler: function (e) {
+            var self = this,
+                product = self.$.product;
+
+            var selectedConfiguration = self.$.selectedConfiguration;
+
+            if (selectedConfiguration && product) {
+
+                var deltaX = 0,
+                    deltaY = 0;
+
+                switch (e.keyCode) {
+                    case 40:
+                        deltaY = 1;
+                        break;
+                    case 38:
+                        deltaY = -1;
+                        break;
+                    case 37:
+                        deltaX = -1;
+                        break;
+                    case 39:
+                        deltaX = 1;
+                }
+
+                if (deltaX || deltaY) {
+
+                    if (e.shiftKey) {
+                        deltaX *= 10;
+                        deltaY *= 10;
+                    }
+
+                    var offset = selectedConfiguration.$.offset;
+                    offset.set({
+                        x: offset.$.x + deltaX,
+                        y: offset.$.y + deltaY
+                    });
+                    selectedConfiguration.set('offset', offset);
+
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+
+
+                if (e.keyCode === 8 || e.keyCode === 46) {
+                    // backspace || delete --> remove selected configuration
+
+                    product.$.configurations.remove(selectedConfiguration);
+                    self.set('selectedConfiguration', null);
+
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
             }
+
         },
 
         _bindDomEvents: function () {
@@ -27,64 +82,13 @@ define(["js/ui/View"], function(View) {
 
                 this.bind("on:click", this._clickHandler, this);
 
-                this.bindDomEvent("keydown", function (e) {
-                    var product = self.$.product;
-
-                    var selectedConfiguration = self.$.selectedConfiguration;
-
-                    if (selectedConfiguration && product) {
-
-                        var deltaX = 0,
-                            deltaY = 0;
-
-                        switch (e.keyCode) {
-                            case 40:
-                                deltaY = 1;
-                                break;
-                            case 38:
-                                deltaY = -1;
-                                break;
-                            case 37:
-                                deltaX = -1;
-                                break;
-                            case 39:
-                                deltaX = 1;
-                        }
-
-                        if (deltaX || deltaY) {
-
-                            if (e.shiftKey) {
-                                deltaX *= 10;
-                                deltaY *= 10;
-                            }
-
-                            var offset = selectedConfiguration.$.offset;
-                            offset.set({
-                                x: offset.$.x + deltaX,
-                                y: offset.$.y + deltaY
-                            });
-                            selectedConfiguration.set('offset', offset);
-
-                            e.preventDefault();
-                        }
-
-
-                        if (e.keyCode === 8 || e.keyCode === 46) {
-                            // backspace || delete --> remove selected configuration
-
-                            product.$.configurations.remove(selectedConfiguration);
-                            self.set('selectedConfiguration', null);
-
-                            e.preventDefault();
-                        }
-                    }
-
+                this.bindDomEvent("keydown", function(e) {
+                    self._keyDownHandler(e);
                 });
 
+                this.callBase();
             }
 
-            this.callBase();
         }
-
     });
 });
