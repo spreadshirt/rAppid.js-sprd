fb4c8bf00015d64e9a1ec7256c533745899698fe
Tony Findeisen <github@it-ony.de>
2013-02-20 20:58:37 +0100

    simplified loadFonts method

--- sprd/view/svg/TextConfigurationRendererClass.js
+++ sprd/view/svg/TextConfigurationRendererClass.js
@@ -24,37 +24,19 @@ define(['sprd/view/svg/ConfigurationRenderer', 'underscore'], function(Configura
         },
 
         _loadFonts: function() {
-            var fonts = this.getFonts(),
+
+            var configuration = this.$.configuration;
+
+            if (!configuration) {
+                return;
+            }
+
+            var fonts = configuration.getUsedFonts(),
                 svgRoot = this.getSvgRoot();
 
             for (var i = 0; i < fonts.length; i++) {
                 var font = fonts[i];
                 svgRoot.fontManager.loadExternalFont(font.getUniqueFontName(), "fonts/svg/" + font.$.id + ".svg#font");
-            }
-        },
-
-        getFonts: function() {
-            var fonts = [];
-
-            var configuration = this.$.configuration;
-            if (configuration && configuration.$.textFlow) {
-                addFonts(configuration.$.textFlow);
-            }
-
-            return fonts;
-
-            function addFonts(flowElement) {
-                if (flowElement) {
-                    var font = flowElement.get("style.font");
-
-                    if (font && _.indexOf(fonts, font) === -1) {
-                        fonts.push(font);
-                    }
-
-                    !flowElement.isLeaf && flowElement.$.children.each(function(child) {
-                        addFonts(child);
-                    });
-                }
             }
         },
 
