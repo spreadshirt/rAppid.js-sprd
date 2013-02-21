64489c937199761393fa0bf285f213fbd216b42c
Marcus Krejpowicz <krebbl@gmail.com>
2013-02-21 11:11:06 +0100

    removed Articles as Collection

--- sprd/collection/Articles.js
+++
@@ -1,6 +0,0 @@
-define(["js/data/Collection"], function(Collection) {
-    return Collection.inherit("sprd.collection.Articles", {
-        // TODO: use this collection during reference resolving
-        // TODO: always fetch currency with article
-    });
-});
