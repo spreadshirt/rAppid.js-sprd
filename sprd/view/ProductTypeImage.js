db3a243fbce7667cf1025b78b6d0b78c3e9b7cac
Marcus Krejpowicz <krebbl@gmail.com>
2013-03-20 17:53:33 +0100

    fixed product image

--- sprd/view/ProductTypeImage.js
+++ sprd/view/ProductTypeImage.js
@@ -1,6 +1,9 @@
 define([
-    'xaml!sprd/view/Image'
-], function (Image) {
+    'xaml!sprd/view/Image',
+    "sprd/data/ImageService"
+], function (Image, ImageService) {
+
+    var viewIdExtractor = /\/views\/(\d+)/;
 
     return Image.inherit('app.view.ProductTypeImage', {
 
@@ -12,17 +15,61 @@ define([
             view: null
         },
 
+        inject: {
+            imageService: ImageService
+        },
+
         imageUrl: function () {
-            if (this.$.productType && this.$.productType.$.resources) {
-                var url;
-                if (this.$.type === "preview") {
-                    url = this.$.productType.$.resources[0].href;
-                } else if (this.$.type === "size") {
-                    url = this.$.productType.$.resources[1].href;
+            var url = null;
+
+            if (this.$.productType) {
+                var imageService = this.$.imageService;
+
+                var productType = this.$.productType;
+
+                if (this.$.type == "preview") {
+                    url = imageService.$.endPoint + "/productTypes/" + productType.$.id;
+                } else {
+                    url = imageService.$.endPoint + "/compositions/" + productType.$.id;
                 }
-                return this.extendUrlWithSizes(url);
+
+
+                var viewId = this.$.view ? this.$.view.$.id : null;
+                if(!viewId){
+                    var view = productType.getDefaultView();
+                    if(view){
+                        viewId = view.$.id;
+                    }
+                }
+
+                var resources = productType.$.resources;
+
+                if (!viewId && resources instanceof Array) {
+                    // get view id from resource -> this is a hack, because image server
+                    // can't generate an image without the view id -> should be implemented by image server
+
+                    for (var i = 0; i < resources.length; i++) {
+                        viewId = viewIdExtractor.exec(resources[i].href);
+                        if (viewId) {
+                            viewId = viewId[1];
+                            break;
+                        }
+                    }
+
+                }
+
+                url += '/views/' + viewId;
+
+
+                if (this.$.appearance) {
+                    url += "/appearances/" + this.$.appearance.$.id;
+                }
+
+                url = this.extendUrlWithSizes(url);
+
+                return url;
             }
-            return "";
+            return url;
         }.onChange('width', 'height', 'productType'),
 
         alt: function () {
