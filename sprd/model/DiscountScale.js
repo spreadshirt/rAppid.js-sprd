a6c281aff7414332b0703ef2ddd479db44df8d84
Marcus Krejpowicz <krebbl@gmail.com>
2013-07-08 15:50:42 +0200

    added getDiscountById to DiscountScale

--- sprd/model/DiscountScale.js
+++ sprd/model/DiscountScale.js
@@ -14,7 +14,23 @@ define(["sprd/data/SprdModel", "js/data/Entity"], function (Model, Entity) {
                 return this.$.discounts.at(this.$.discounts.size() - 1);
             }
             return "";
-        }.onChange("discounts")
+        }.onChange("discounts"),
+
+        getDiscountById: function (discountId) {
+            var currentDiscount = null;
+            if (discountId && this.$.discounts) {
+                this.$.discounts.each(function (discountItem) {
+                    if (discountItem.$.id === discountId) {
+                        currentDiscount = discountItem;
+                        this["break"]();
+                    }
+                });
+            }
+
+            return currentDiscount;
+        }.onChange('discounts')
+
     });
 
-});
+})
+;
