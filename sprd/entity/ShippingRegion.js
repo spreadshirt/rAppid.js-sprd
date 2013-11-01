f415c7937aae431ee839b53c6e7ea9f8ad002f8d
Tony Findeisen <github@it-ony.de>
2013-11-01 17:24:02 +0100

    added getShippingCostForOrderValue to ShippingRegion

--- sprd/entity/ShippingRegion.js
+++ sprd/entity/ShippingRegion.js
@@ -3,7 +3,26 @@ define(["js/data/Entity", "sprd/entity/ShippingCost"], function (Entity, Shippin
 
         schema: {
             shippingCosts: [ShippingCost]
-        }
+        },
+
+        getShippingCostsForOrderValue: function(value) {
+
+            var shippingCosts = this.$.shippingCosts;
+            if (shippingCosts) {
+                for (var i = 0; i < shippingCosts.$items.length; i++) {
+                    var shippingCost = shippingCosts.$items[i],
+                        orderValueRange = shippingCost.$.orderValueRange;
+
+
+                    if (value >= orderValueRange.$.from && (orderValueRange.$.to === null || value <= orderValueRange.$.to)) {
+                        // found the cost
+                        return shippingCost;
+                    }
+                }
+            }
+
+            return null
+        },
 
     });
 });
