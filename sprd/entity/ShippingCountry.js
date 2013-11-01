007aae4065cb2f89b0a46affdc9932eb4512dae4
Tony Findeisen <github@it-ony.de>
2013-11-01 15:12:26 +0100

    defined defaults for Shippingcountry

--- sprd/entity/ShippingCountry.js
+++ sprd/entity/ShippingCountry.js
@@ -1,11 +1,23 @@
 define(["js/data/Entity", "sprd/entity/ShippingRegion", "sprd/entity/ShippingState"], function (Entity, ShippingRegion, ShippingState) {
     return Entity.inherit("sprd.entity.ShippingCountry", {
 
+        defaults: {
+            name: null,
+            isoCode: null,
+            shippingRegion: null,
+            externalFulfillmentSupported: true,
+            trackingLink: null
+        },
+
         schema: {
             name: String,
             isoCode: String,
-            shippingRegion: ShippingRegion,
+            shippingRegion: {
+                isReference: true,
+                type: ShippingRegion
+            },
             externalFulfillmentSupported: Boolean,
+            trackingLink: Object,
             shippingStates: [ShippingState]
         }
 
