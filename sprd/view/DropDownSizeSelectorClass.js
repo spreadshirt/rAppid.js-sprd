26f64f1e6ad35c8cfef8d686114d813f9ac09f85
it-ony <git@tonyfindeisen.de>
2014-01-16 11:53:05 +0100

    rewritten drop down selector to choose first size correctly, even if more size are available, but stocked out

--- sprd/view/DropDownSizeSelectorClass.js
+++ sprd/view/DropDownSizeSelectorClass.js
@@ -1,22 +1,63 @@
-define(["xaml!js/ui/MenuButton"], function(MenuButton) {
+define(["xaml!js/ui/MenuButton"], function (MenuButton) {
     return MenuButton.inherit({
 
         $defaultContentName: null,
 
         defaults: {
-            type: "{btnType()}"
+            type: "{btnType()}",
+
+            /***
+             * the product type to show sizes for
+             * @type sprd.model.ProductType
+             */
+            productType: null,
+
+            /***
+             * the selected appearance of the product type
+             * @type sprd.entity.Appearance
+             */
+            appearance: null,
+
+            /***
+             * the selected size
+             * @type sprd.entity.ProductTypeSize
+             */
+            selectedSize: null,
+
+            _sizes: null
         },
 
         initialize: function () {
             this.callBase();
-            this.bind('change:productType', this._onProductTypeChange, this);
+
+            this.bind('productType.stockStates', 'reset', this._buildSizeList, this);
         },
 
-        _onProductTypeChange: function () {
-            var sizes = this.get('productType.sizes');
-            if (sizes && sizes.length === 1) {
-                this.set('selectedSize', sizes.at(0));
+        _commitChangedAttributes: function ($) {
+
+            if (this._hasSome($, ["productType", "appearance"])) {
+                this._buildSizeList();
             }
+
+            this.callBase();
+        },
+
+        _buildSizeList: function() {
+            var sizes;
+
+            var appearance = this.$.appearance,
+                productType = this.$.productType;
+
+            if (productType && appearance) {
+                sizes = productType.getAvailableSizesForAppearance(appearance);
+            }
+
+            this.set("_sizes", sizes);
+
+            if (sizes && sizes.length === 1) {
+                this.set("selectedSize", sizes.at(0));
+            }
+
         },
 
         closeMenu: function () {
