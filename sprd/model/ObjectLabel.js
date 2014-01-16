cd672c64921fa2daf368a534770a1c8594881229
it-ony <git@tonyfindeisen.de>
2014-01-16 15:30:32 +0100

    corrected fqClassName in ObjectLabel

--- sprd/model/ObjectLabel.js
+++ sprd/model/ObjectLabel.js
@@ -1,5 +1,5 @@
 define(['sprd/data/SprdModel', 'sprd/model/Label', 'sprd/model/ObjectType', 'sprd/model/LabelObject'], function (SprdModel, Label, ObjectType, LabelObject) {
-    return SprdModel.inherit('sprd.model.LabelType', {
+    return SprdModel.inherit('sprd.model.ObjectLabel', {
         schema: {
             label: Label,
             type: ObjectType,
