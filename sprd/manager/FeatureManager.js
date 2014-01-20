7a4d9fcf042dfdaeb6e656a0f12138126aae2993
it-ony <git@tonyfindeisen.de>
2014-01-20 10:59:04 +0100

    added possibility to set features via url parameter

--- sprd/manager/FeatureManager.js
+++ sprd/manager/FeatureManager.js
@@ -11,6 +11,29 @@ define(['js/core/Component'], function(Component){
             this.set(featureSets[name] || {});
         },
 
+        _initializationComplete: function() {
+
+            this.callBase();
+
+            // read features from url
+            if (!this.runsInBrowser()) {
+                return;
+            }
+
+            var search = window.location.search.replace(/^\?/, "").split("&");
+            for (var i = 0; i < search.length; i++) {
+                var keyValue = search[i].split("="),
+                    key = keyValue.shift(),
+                    value = keyValue.join("=");
+
+                if (this.$.hasOwnProperty(key)) {
+                    // we react on this key
+                    this.set(key, value);
+                }
+            }
+
+        },
+
         getFeatureState: function(feature){
             return this.get(feature);
         }.on('change'),
