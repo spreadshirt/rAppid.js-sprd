6b54aabaa8c2f16ed1fc39c622c47b1b7e083c24
it-ony <git@tonyfindeisen.de>
2014-01-28 13:55:07 +0100

    DEV-61084 - Can only view detail page of one article in one session

--- sprd/collection/StockStates.js
+++ sprd/collection/StockStates.js
@@ -4,6 +4,11 @@ define(["js/data/Collection", "sprd/model/StockState"], function(Collection, Sto
         $modelFactory: StockState,
 
         isSizeAndAppearanceAvailable: function(size, appearance){
+
+            if (!(size && appearance)) {
+                return false;
+            }
+
             return this.find(function(item){
                 return item.$.size.$.id === size.$.id && item.$.appearance.$.id === appearance.$.id;
             }) || false;
