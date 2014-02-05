aca0dcca6542c814f830f7a830c6bbb209cf5697
Chris <chrl@spreadshirt.net>
2014-02-05 15:07:53 +0100

    DEV-61373 - Refactoring MessageDialog.

--- sprd/window/MessageDialogClass.js
+++ sprd/window/MessageDialogClass.js
@@ -1,27 +1,11 @@
 define(["xaml!js/ui/Dialog"], function (Dialog) {
     return Dialog.inherit("sprd.window.MessageDialog", {
         defaults: {
-            buttons: [{
-                "label": "Ok",
-                "classes": "dialog-btn btn-success"
-            }, {
-                "label": "Cancel",
-                "classes": "dialog-btn"
-            }],
-            classes: null,
-            closable: "false",
-            closeOnBackdrop: "true",
+            buttons: null,
+            closable: false,
+            closeOnBackdrop: true,
             title: null,
             message: null
-        },
-
-        createDialog: function (dialogParams) {
-            this.set("title", dialogParams.title);
-            this.set("message", dialogParams.message);
-            this.set("buttons", [this.$.buttons[dialogParams.buttons]]);
-            this.set("classes", dialogParams.classes);
-
-            this.showModal();
         }
     });
 });
