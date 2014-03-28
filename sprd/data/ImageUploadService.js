d714b253d12e9eb8ce95485ebcb6baf33b11c15d
marcel pflug <mapf@spreadshirt.net>
2014-03-28 09:54:34 +0100

    - build alternative iframe upload

--- sprd/data/ImageUploadService.js
+++ sprd/data/ImageUploadService.js
@@ -22,6 +22,8 @@ define(["js/core/Component", "xaml!sprd/data/ImageServerDataSource", "flow", "sp
 
                 if (data instanceof Image) {
                     image = data;
+                } else if (data.$el && data.$el instanceof HTMLInputElement) {
+                    image = data;
                 } else if (_.isString(data)) {
                     image = new RemoteImage({
                         src: data
@@ -43,6 +45,8 @@ define(["js/core/Component", "xaml!sprd/data/ImageServerDataSource", "flow", "sp
 
 
             _uploadDesign: function (uploadDesign, restrictions, callback) {
+                var self = this;
+
                 if (restrictions instanceof Function) {
                     callback = restrictions;
                     restrictions = null;
@@ -114,7 +118,9 @@ define(["js/core/Component", "xaml!sprd/data/ImageServerDataSource", "flow", "sp
                                     uploadDesign.set('uploadProgress', 100);
                                 };
 
-                            }
+                            },
+                            imageServerEndPoint: self.get('imageServer.endPoint'),
+                            apiKey: self.get('imageServer.apiKey')
                         }, cb);
                     })
                     .exec(function (err) {
