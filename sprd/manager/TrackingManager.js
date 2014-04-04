bd6b83eaed3e4ba7791f82fcce867f431595c46e
Tony Findeisen <github@it-ony.de>
2014-04-04 13:59:32 +0200

    DEV-64393 - Uploaded design doesn't show up

--- sprd/manager/TrackingManager.js
+++ sprd/manager/TrackingManager.js
@@ -1,8 +1,23 @@
 define(["js/core/Component"], function (Component) {
-    return Component.inherit("sprd.manager.TrackingManagerBase", {
+    return Component.inherit("sprd.manager.TrackingManager", {
         ctor: function () {
             this.$uniqueId = (new Date()).getTime();
             this.callBase();
+        },
+
+        trackUploadDesignCreationFailed: function(err) {
+            var google = this.$.google;
+            google && google.trackEvent("Upload", "DesignCreationFailed", this.get(err, "xhr.responses.text"));
+        },
+
+        trackUploadFailed: function(err) {
+            var google = this.$.google;
+            google && google.trackEvent("Upload", "Failed", (err || "").toString());
+        },
+
+        trackUploadSuccess: function() {
+            var google = this.$.google;
+            google && google.trackEvent("Upload", "Success");
         }
     });
 });
