3ef50ab0a32c472575174dc2289d4419f308b7bf
marcel pflug <mapf@spreadshirt.net>
2014-04-07 11:50:48 +0200

    DEV-64272 -  [FE] Make uploader work for IE8+ => setup iframe only when possible

--- sprd/ui/FileUploadClass.js
+++ sprd/ui/FileUploadClass.js
@@ -14,22 +14,25 @@ define(['js/ui/FileUpload', 'sprd/data/IframeUpload'], function(FileUpload, Ifra
                 iframe.bind('on:load', function (e) {
                     var iframeInput,
                         iframeDocument = e.target.$el.contentDocument,
+                        iframeBody;
+
+                    if (iframeDocument) {
                         iframeBody = iframeDocument.getElementsByTagName('body')[0];
+                        iframeContent = self.$templates.iframeContent.createInstance();
 
-                    iframeContent = self.$templates.iframeContent.createInstance();
+                        iframeBody.appendChild(iframeContent.render());
 
-                    iframeBody.appendChild(iframeContent.render());
+                        iframeInput = self.$.iframeInput;
 
-                    iframeInput = self.$.iframeInput;
+                        iframeInput.bind('on:change', function () {
+                            self.trigger('on:change', { iframeUpload: new IframeUpload(self) } );
+                        });
 
-                    iframeInput.bind('on:change', function () {
-                        self.trigger('on:change', { iframeUpload: new IframeUpload(self) } );
-                    });
-
-                    self.bind('on:click', function (ev) {
-                        self.$.iframeInput.$el.click();
-                        ev.preventDefault();
-                    });
+                        self.bind('on:click', function (ev) {
+                            self.$.iframeInput.$el.click();
+                            ev.preventDefault();
+                        });
+                    }
                 });
 
                 this.$stage.addChild(iframe);
