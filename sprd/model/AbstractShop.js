181ddbed0f22c88163e3c2e8e8008d5e6c3b3ead
Tony Findeisen <github@it-ony.de>
2014-05-28 15:53:04 +0200

    Refactored Shop Model to load different necessary dependencies for different applications

--- sprd/model/AbstractShop.js
+++ sprd/model/AbstractShop.js
@@ -1,48 +1,33 @@
-define([
-    "sprd/data/SprdModel",
-    'js/data/Collection',
-    'sprd/model/Basket',
-    'sprd/model/Currency',
-    'sprd/model/Address',
-    'sprd/model/Design',
-    'sprd/model/ProductType',
-    'sprd/model/ProductTypeDepartment',
-    'sprd/model/ArticleCategory',
-    'sprd/model/DesignCategory',
-    'sprd/model/Article', 'sprd/model/Country', 'sprd/model/PrintType', 'sprd/model/FontFamily', "sprd/model/Product", "sprd/model/DiscountScale", 'sprd/model/Application', 'sprd/model/ShippingType'],
-    function (SprdModel, Collection, Basket, Currency, Address, Design, ProductType, ProductTypeDepartment, ArticleCategory, DesignCategory, Article, Country, PrintType, FontFamily, Product, DiscountScale, Application, ShippingType) {
+define(['sprd/data/SprdModel', 'js/data/Collection', 'sprd/model/Currency', 'sprd/model/Address', 'sprd/model/Country'], function (SprdModel, Collection, Currency, Address, Country) {
 
-        return SprdModel.inherit("sprd.model.AbstractShop", {
-            schema: {
+    return SprdModel.inherit("sprd.model.AbstractShop", {
 
+        defaults: {
+            platform: null
+        },
 
-                country: Country,
-//            language: SprdModel,
-                currency: Currency,
-//            address: SprdModel,
-                address: Address,
-                productTypes: Collection.of(ProductType),
-                printTypes: Collection.of(PrintType),
-                fontFamilies: Collection.of(FontFamily),
-                productTypeDepartments: Collection.of(ProductTypeDepartment),
-            shippingTypes: Collection.of(ShippingType),
-            designCategories: Collection.of(DesignCategory),
-                designs: Collection.of(Design),
-                articleCategories: Collection.of(ArticleCategory),
-                articles: Collection.of(Article),
-                discountSupported: Boolean,
-                discountScale: DiscountScale,
-                products: Collection.of(Product),
-                currencies: Collection.of(Currency),
-//            languages: Collection,
-                countries: Collection.of(Country),
-                baskets: Collection.of(Basket),
-                applications: Collection.of(Application),
-                name: String,
-                description: String,
-                user: "sprd/model/User"
+        schema: {
 
-            }
-        });
+            address: Address,
+            country: Country,
+            currency: Currency,
+
+            currencies: Collection.of(Currency),
+            countries: Collection.of(Country),
+
+            name: String,
+            description: String,
+            user: "sprd/model/User",
+            discountSupported: Boolean
+
+        },
+
+        isMarketPlace: function () {
+            var platform = this.$.platform,
+                id = this.$.id;
+            return (platform === "EU" && id == 205909) || (platform === "NA" && id == 93439);
+        }.onChange("platform", "id")
+
     });
+});
 
