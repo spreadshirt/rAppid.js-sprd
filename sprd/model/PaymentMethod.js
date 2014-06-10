34d0e8dd3ca418b71cd4b5181cfb31b758cbf9cd
Tony Findeisen <github@it-ony.de>
2014-06-10 15:50:07 +0200

    created schema for PaymentMethod

--- sprd/model/PaymentMethod.js
+++ sprd/model/PaymentMethod.js
@@ -1,12 +1,16 @@
 define(["sprd/data/SprdModel"], function (SprdModel) {
-    return SprdModel.inherit("sprd.mode.PaymentMethod", {
+    return SprdModel.inherit("sprd.model.PaymentMethod", {
 
         defaults: {
 
         },
 
         schema: {
-
+            weight: Number,
+            type: String,
+            subType: String,
+            grossLowerLimit: Number,
+            grossUpperLimit: Number
         }
 
     });
