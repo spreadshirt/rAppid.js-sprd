26ce0761ab286372a9261d4716336d184e40a155
Tony Findeisen <github@it-ony.de>
2014-06-10 17:22:44 +0200

    overwrite save method for checkout

--- sprd/model/Checkout.js
+++ sprd/model/Checkout.js
@@ -13,8 +13,33 @@ define(["sprd/data/SprdModel", "sprd/entity/Payment"], function(SprdModel, Payme
 
         schema: {
             paymentMethod: PaymentMethod,
-            payment: Payment,
+            // TODO: find the circular dependency
+            payment: "sprd/entity/Payment",
             links: Object
+        },
+
+        save: function() {
+
+            var dataSource = this.$context.$dataSource;
+            var url = this._buildUriForResource(this);
+
+            var processor = dataSource.getProcessorForModel(this);
+            var formatProcessor = dataSource.getFormatProcessor("save", this);
+
+            var form = document.createElement("form");
+            form.setAttribute("method", "post");
+            form.setAttribute("action", url);
+
+            var valueField = document.createElement("input");
+            valueField.setAttribute("type", "hidden");
+            valueField.setAttribute("name", "value");
+            valueField.setAttribute("enctype", "application/x-www-form-urlencoded");
+            valueField.setAttribute("value", formatProcessor.serialize(processor.compose(this, "save")));
+
+            form.appendChild(valueField);
+            document.body.appendChild(form);
+            form.submit();
+
         }
 
     });
