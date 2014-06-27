7964c7738e98a593137ed90bb354ab7f3f2b97d1
Tony Findeisen <github@it-ony.de>
2014-06-27 15:05:14 +0200

    added default to Payment

--- sprd/entity/Payment.js
+++ sprd/entity/Payment.js
@@ -2,6 +2,10 @@ define(["js/data/Entity"], function(Entity) {
     return Entity.inherit("checkout.entity.Payment", {
         type: "payment",
 
+        defaults: {
+            root: null
+        },
+
         getType: function() {
             return this.type;
         }
