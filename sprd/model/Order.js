ddf0dfe1e204e38a6f906648303c72002eb16ac6
Tony Findeisen <github@it-ony.de>
2014-07-24 15:06:44 +0200

    billing is now part of an Order

--- sprd/model/Order.js
+++ sprd/model/Order.js
@@ -1,9 +1,10 @@
-define(["sprd/data/SprdModel", "sprd/model/Shop", "sprd/model/OrderItem", "js/data/Collection"], function (SprdModel, Shop, OrderItem, Collection) {
+define(["sprd/data/SprdModel", "sprd/model/Shop", "sprd/model/OrderItem", "js/data/Collection", "sprd/model/Delivery"], function (SprdModel, Shop, OrderItem, Collection, Delivery) {
     return SprdModel.inherit('sprd.model.Order', {
 
         schema: {
             shop: Shop,
-            orderItems: Collection.of(OrderItem)
+            orderItems: Collection.of(OrderItem),
+            billing: Delivery.Billing
         }
 
     });
