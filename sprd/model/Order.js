744f4f268876fdf56d7ed4b95b15682e42be5634
Marcus Krejpowicz <krebbl@gmail.com>
2014-08-06 14:33:39 +0200

    DEV-69749 - Include Trusted shops in order confirmation * added trusted shop

--- sprd/model/Order.js
+++ sprd/model/Order.js
@@ -1,11 +1,36 @@
-define(["sprd/data/SprdModel", "sprd/model/Shop", "sprd/model/OrderItem", "js/data/Collection", "sprd/model/Delivery"], function (SprdModel, Shop, OrderItem, Collection, Delivery) {
-    return SprdModel.inherit('sprd.model.Order', {
+define(["sprd/data/SprdModel", "sprd/model/Shop", "sprd/model/OrderItem", "js/data/Collection", "sprd/model/Delivery", "sprd/entity/Price"], function (SprdModel, Shop, OrderItem, Collection, Delivery, Price) {
+    var Shipping = Delivery.Shipping.inherit("sprd.model.Order.Shipping", {
+        schema: {
+            price: Price
+        }
+    });
 
+    return SprdModel.inherit('sprd.model.Order', {
+        defaults: {
+            shipping: null,
+            orderItems: null
+        },
         schema: {
             shop: Shop,
             orderItems: Collection.of(OrderItem),
-            billing: Delivery.Billing
-        }
+            billing: Delivery.Billing,
+            shipping: Shipping
+        },
+
+        totalVatIncluded: function () {
+            var vatIncluded = 0;
+            if (this.$.orderItems) {
+                this.$.orderItems.each(function (orderItem) {
+                    vatIncluded += orderItem.totalVatIncluded();
+                });
+            }
+            var shippingPrice = this.get('shipping.price');
+            if (shippingPrice) {
+                vatIncluded += shippingPrice.$.vatIncluded;
+            }
+
+            return vatIncluded;
+        }.onChange('orderItems', 'shipping.price')
 
     });
 });
