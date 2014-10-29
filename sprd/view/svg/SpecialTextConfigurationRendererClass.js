0de58f4dd8f0853c98d80475868e9880e2b09b9d
Tony Findeisen <github@it-ony.de>
2014-10-29 16:31:59 +0100

    fixed fqClassname of SpecialTextConfigurationRendererClass

--- sprd/view/svg/SpecialTextConfigurationRendererClass.js
+++ sprd/view/svg/SpecialTextConfigurationRendererClass.js
@@ -1,6 +1,6 @@
 define(['sprd/view/svg/ConfigurationRenderer'], function (ConfigurationRenderer) {
 
-    return ConfigurationRenderer.inherit("sprd.view.svg.SpecialTextConfigurationRenderer", {
+    return ConfigurationRenderer.inherit("sprd.view.svg.SpecialTextConfigurationRendererClass", {
 
         defaults: {
             _href: "{configuration.previewImageUrl}",
