764fc02140c59f5c13142e58502fff5c2572179a
Marcus Krejpowicz <krebbl@gmail.com>
2015-02-26 14:51:33 +0100

    DEV-79005 - Converting DT to DD does not work for vector designs

--- sprd/manager/ProductManager.js
+++ sprd/manager/ProductManager.js
@@ -174,25 +174,26 @@ define(["sprd/manager/IProductManager", "underscore", "flow", "sprd/util/Product
                             y: configuration.$.offset.$.y + currentConfigurationHeight / 2
                         };
 
+                        // if digital print type
+                        if (!printType.isPrintColorColorSpace()) {
+                            var pt;
+                            // try to find another digital print type which is before the current print type
+                            // this is needed to switch back from DD to DT
+                            for (var k = 0; k < possiblePrintTypes.length; k++) {
+                                pt = possiblePrintTypes[k];
+                                if (!pt.isPrintColorColorSpace()) {
+                                    printType = pt;
+                                    break;
+                                }
+                            }
+                        }
+
                         if (printType && !_.contains(possiblePrintTypes, printType)) {
                             // print type not possible any more
                             printType = null;
                         }
 
                         if (printType) {
-                            // if digital print type
-                            if (!printType.isPrintColorColorSpace()) {
-                                var pt;
-                                // try to find another digital print type which is before the current print type
-                                // this is needed to switch back from DD to DT
-                                for (var k = 0; k < possiblePrintTypes.length; k++) {
-                                    pt = possiblePrintTypes[k];
-                                    if (!pt.isPrintColorColorSpace()) {
-                                        printType = pt;
-                                        break;
-                                    }
-                                }
-                            }
                             var index = _.indexOf(possiblePrintTypes, printType);
                             if (index >= 0) {
                                 // remove print type from original position
