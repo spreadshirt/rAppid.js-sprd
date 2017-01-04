3585a32943820237cc0723b7216af314a4d761db
Mohamed Hashi <mhas@spreadshirt.net>
2017-01-04 15:48:29 +0100

    DEV-112231 sorting of masks

--- sprd/config/AfterEffects.js
+++ sprd/config/AfterEffects.js
@@ -1,120 +1,112 @@
-define(["sprd/model/VectorMask", "sprd/model/PixelMask"], function(VectorMask, PixelMask) {
+define(["sprd/model/VectorMask", "sprd/model/PixelMask", "underscore"], function(VectorMask, PixelMask, _) {
     return function(baseUrl) {
+        var masks = [
+            new VectorMask({
+                name: "Heart",
+                vector: baseUrl("sprd/img/masks/heart.svg"),
+                id: 12,
+                fixedAspectRatio: true
+            }),
+            new VectorMask({
+                name: "Circle",
+                vector: baseUrl("sprd/img/masks/circle_black.svg"),
+                id: 8,
+                fixedAspectRatio: false
+            }),
+            new VectorMask({
+                name: "Ellipse",
+                vector: baseUrl("sprd/img/masks/ellipse_black.svg"),
+                id: 9,
+                fixedAspectRatio: false
+            }),
+            new VectorMask({
+                name: "Triangle Rounded",
+                vector: baseUrl("sprd/img/masks/triangle_rounded_black.svg"),
+                id: 3,
+                fixedAspectRatio: false
+            }),
+            new VectorMask({
+                name: "Rectangle #1",
+                vector: baseUrl("sprd/img/masks/rectangle_1_black.svg"),
+                id: 2,
+                fixedAspectRatio: false
+            }),
+            new VectorMask({
+                name: "Rectangle #2",
+                vector: baseUrl("sprd/img/masks/rectangle_2_black.svg"),
+                id: 3,
+                fixedAspectRatio: false
+            }),
+            new VectorMask({
+                name: "Trapezium",
+                vector: baseUrl("sprd/img/masks/trapezium_black.svg"),
+                id: 7,
+                fixedAspectRatio: false
+            }),
+            new VectorMask({
+                name: "S",
+                vector: baseUrl("sprd/img/masks/SlikeSusi_black.svg"),
+                id: 9,
+                fixedAspectRatio: false
+            }),
+            new VectorMask({
+                name: "A",
+                vector: baseUrl("sprd/img/masks/AlikeAnton_black.svg"),
+                id: 13,
+                fixedAspectRatio: false
+            }),
+            new VectorMask({
+                name: "Star",
+                vector: baseUrl("sprd/img/masks/starrounded_black.svg"),
+                id: 10,
+                fixedAspectRatio: false
+            }),
+            new VectorMask({
+                name: "Karo",
+                vector: baseUrl("sprd/img/masks/karo_black.svg"),
+                id: 6,
+                fixedAspectRatio: false
+            }),
+            new VectorMask({
+                name: "It's complicated",
+                vector: baseUrl("sprd/img/masks/its_complicated_black.svg"),
+                id: 14,
+                fixedAspectRatio: false
+            }),
+            new VectorMask({
+                name: "Polygon",
+                vector: baseUrl("sprd/img/masks/polygon_black.svg"),
+                id: 5,
+                fixedAspectRatio: false
+            }),
+            new VectorMask({
+                name: "Rounded Square",
+                vector: baseUrl("sprd/img/masks/roundedsquare_black.svg"),
+                id: 1,
+                fixedAspectRatio: false
+            }),
+            new VectorMask({
+                name: "Rounded",
+                vector: baseUrl("sprd/img/masks/rounded_black.svg"),
+                id: 11,
+                fixedAspectRatio: false
+            }),
+            new PixelMask({
+                name: "Große Kreuz",
+                image: baseUrl("sprd/img/masks/03-grosses-kreuz.png"),
+                id: 15
+            }),
+            new PixelMask({
+                name: "Rand",
+                image: baseUrl("sprd/img/masks/04-Rand.png"),
+                id: 17
+            })
+        ];
+
         return {
-            masks: [
-                new VectorMask({
-                    name: "Heart",
-                    vector: baseUrl("sprd/img/masks/heart.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/12_heart.png"),
-                    id: 12
-                }),
-                new VectorMask({
-                    name: "Circle",
-                    vector: baseUrl("sprd/img/masks/circle_black.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/08_circle.png"),
-                    id: 8
-                }),
-                new VectorMask({
-                    name: "Ellipse",
-                    vector: baseUrl("sprd/img/masks/ellipse_black.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/09_ellipse.png"),
-                    id: 9
-                }),
-                new VectorMask({
-                    name: "Triangle Rounded",
-                    vector: baseUrl("sprd/img/masks/triangle_rounded_black.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/04_triangle_rounded.png"),
-                    id: 3
-                }),
-                // new VectorMask({
-                //     name: "Line",
-                //     vector: baseUrl("sprd/img/masks/line_black.svg"),
-                //     id: 5
-                // }),
-                new VectorMask({
-                    name: "Rectangle #1",
-                    vector: baseUrl("sprd/img/masks/rectangle_1_black.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/02_ rectangle_1.png"),
-                    id: 2
-                }),
-                new VectorMask({
-                    name: "Rectangle #2",
-                    vector: baseUrl("sprd/img/masks/rectangle_2_black.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/03_rectangle_2.png"),
-                    id: 3
-                }),
-                new VectorMask({
-                    name: "Trapezium",
-                    vector: baseUrl("sprd/img/masks/trapezium_black.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/07_trapezium.png"),
-                    id: 7
-                }),
-                new VectorMask({
-                    name: "S",
-                    vector: baseUrl("sprd/img/masks/SlikeSusi_black.svg"),
-                    id: 9
-                }),
-                new VectorMask({
-                    name: "A",
-                    vector: baseUrl("sprd/img/masks/AlikeAnton_black.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/13_AlikeAnton.png"),
-                    id: 13
-                }),
-                new VectorMask({
-                    name: "Star",
-                    vector: baseUrl("sprd/img/masks/starrounded_black.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/10_starrounded.png"),
-                    id: 10
-                }),
-                new VectorMask({
-                    name: "Karo",
-                    vector: baseUrl("sprd/img/masks/karo_black.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/06_karo.png"),
-                    id: 6
-                }),
-                new VectorMask({
-                    name: "It's complicated",
-                    vector: baseUrl("sprd/img/masks/its_complicated_black.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/14_itscomlicated.png"),
-                    id: 14
-                }),
-                new VectorMask({
-                    name: "Polygon",
-                    vector: baseUrl("sprd/img/masks/polygon_black.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/05_polygon.png"),
-                    id: 5
-                }),
-                new VectorMask({
-                    name: "Rounded Square",
-                    vector: baseUrl("sprd/img/masks/roundedsquare_black.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/01_roundedsquare.png"),
-                    id: 1
-                }),
-                new VectorMask({
-                    name: "Rounded",
-                    image: baseUrl("sprd/img/masks/rounded_black.svg"),
-                    preview: baseUrl("sprd/img/masks/previews/11_rounded.png"),
-                    id: 11
-                }),
-                new PixelMask({
-                    name: "Große Kreuz",
-                    image: baseUrl("sprd/img/masks/03-grosses-kreuz.png"),
-                    preview: baseUrl("sprd/img/masks/previews/15-cross.png"),
-                    id: 15
-                }),
-                new PixelMask({
-                    name: "Rand",
-                    image: baseUrl("sprd/img/masks/04-Rand.png"),
-                    preview: baseUrl("sprd/img/masks/previews/17_marker01.png"),
-                    id: 17
-                })
-            ]
-            // ,
-            // frames: [
-            //     new Frame({
-            //         name: "GrungeFrame",
-            //         image: "sprd/img/masks/grunge-frame-2.png"
-            //     })]
+            masks: _.sortBy(masks, function(mask) {
+                return mask.$.id;
+            })
         }
     }
 });
