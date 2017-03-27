010eca728d9499809899dbaa49b2d3efad9c08f5
Mohamed Hashi <mhas@spreadshirt.net>
2017-03-27 13:24:52 +0200

    Blurring active element when committing selected configuration. Added null/undefined checks.

--- sprd/view/ProductViewerClass.js
+++ sprd/view/ProductViewerClass.js
@@ -47,7 +47,8 @@ define(["js/ui/View", "js/core/Bus", "sprd/manager/ProductManager", "sprd/data/I
         },
 
         _commitSelectedConfiguration: function (selectedConfiguration, oldSelectedConfiguration) {
-            document.activeElement.blur();
+            var activeElement = document.activeElement;
+            selectedConfiguration && activeElement && activeElement.blur && activeElement.blur();
             if (this.$.product && oldSelectedConfiguration && (oldSelectedConfiguration.type === "text" || oldSelectedConfiguration.type ===  "specialText")) {
                 if (oldSelectedConfiguration.$.isNew) {
                     this.$.product.$.configurations.remove(oldSelectedConfiguration);
