edc0b8756bf74e223d23e620e62a7dc4db128900
Tony Findeisen <github@it-ony.de>
2017-04-07 16:13:52 +0200

    fixed selection / deselection of configurations

--- sprd/view/ScrollIntoViewDomNode.js
+++ sprd/view/ScrollIntoViewDomNode.js
@@ -1,12 +1,20 @@
 define(["js/ui/View", "sprd/lib/ScrollIntoViewIfNeeded"], function(View, ScrollIntoViewIfNeeded) {
     return View.inherit({
 
+        defaults: {
+            scrollDelay: 0
+        },
+
         _renderSelected: function(selected) {
 
             this.callBase();
 
             if (selected) {
-                ScrollIntoViewIfNeeded(this.$el);
+                var $el = this.$el,
+                    self = this;
+                setTimeout(function() {
+                    self.$.selected && ScrollIntoViewIfNeeded($el);
+                }, this.$.scrollDelay)
             }
         }
 
