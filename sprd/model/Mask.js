af41295a8a806accc5a8d2ea545b96c72de7569c
Jonas Bautzmann <joba@spreadshirt.net>
2017-10-06 15:47:48 +0200

    DEV-115682 - Mask settings in Smartomat

--- sprd/model/Mask.js
+++ sprd/model/Mask.js
@@ -80,7 +80,7 @@ define(["sprd/model/AfterEffect", 'sprd/model/MaskApplier', "sprd/model/Design",
                 callback && callback(e);
             };
 
-            img.src = this.relativePreviewUrl();
+            img.src = this.relativePreviewUrl(options.width);
         },
 
         centerAt: function(x, y, options) {
