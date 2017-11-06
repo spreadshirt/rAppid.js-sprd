9517d9c868b839cf4268e1600040eeb727c6c9ae
Tony Findeisen <github@it-ony.de>
2017-11-06 11:10:16 +0100

    removed unused code zoomToPrintAreaFactor

--- sprd/view/ProductViewerSvg.js
+++ sprd/view/ProductViewerSvg.js
@@ -93,7 +93,6 @@ define(['js/svg/Svg', 'sprd/data/ImageService', 'sprd/view/svg/ProductTypeViewVi
 
             _render_view: function (view) {
 
-
                 if (!view) {
                     return
                 }
@@ -108,13 +107,6 @@ define(['js/svg/Svg', 'sprd/data/ImageService', 'sprd/view/svg/ProductTypeViewVi
                 view && this.setViewBox(0, 0, width, height);
                 this._renderProductTypeView(this.$._productType, view);
             },
-
-            zoomToPrintAreaFactor: function() {
-                var view = this.get('_view');
-                var viewMaps = view.$.viewMaps.$items;
-                var surroundingRect = this.surroundingRectForViewMaps(viewMaps);
-                return 1 + (Math.min(view.get('size.width') / surroundingRect.width, view.get('size.height') / surroundingRect.height) - 1) * this.$.productViewer.$.zoomToPrintArea;
-            }.onChange('_view.size.width', '_view.size.height', 'zoomToPrintArea'),
 
             setViewBox: function(x, y, width, height) {
                 this.callBase();
