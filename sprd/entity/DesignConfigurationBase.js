452f877682f2698ad6748b2d3bcb357ccbbd1982
Mohamed Hashi <mhas@spreadshirt.net>
2017-11-06 15:00:44 +0100

    remove trailing comma

--- sprd/entity/DesignConfigurationBase.js
+++ sprd/entity/DesignConfigurationBase.js
@@ -132,7 +132,7 @@ define(['sprd/entity/Configuration', 'sprd/entity/Size', 'sprd/util/UnitUtil', '
                 }
 
                 return /^[\s\n\r]*$/.test(text);
-            },
+            }
         });
 
     }
