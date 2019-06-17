0973dddc91cd884bb57ba25b75508fd1fee3c328
Tony Findeisen <github@it-ony.de>
2019-06-17 23:30:49 +0200

    set type of concrete element for sellables in BasketItemProcessor

--- sprd/model/processor/BasketItemProcessor.js
+++ sprd/model/processor/BasketItemProcessor.js
@@ -80,6 +80,7 @@ define(['sprd/model/processor/DefaultProcessor', 'sprd/model/Shop', 'sprd/model/
                 elementPayload.item = this.$dataSource.getContextForChild(Product, shop).createEntity(Product, element.id);
             } else if (element.type == TYPE_SELLABLE) {
                 elementPayload.item = elementPayload["product"];
+                elementPayload.type = element.type;
             } else {
                 throw "Element type '" + element.type + "' not supported";
             }
