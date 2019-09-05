6b635629925ff138377b83298416c4e6a2e53006
Tony Findeisen <github@it-ony.de>
2019-09-05 08:38:40 +0200

    WIP

--- sprd/model/UserAddress.js
+++ sprd/model/UserAddress.js
@@ -1,5 +1,7 @@
 define(['sprd/data/SprdModel', 'sprd/entity/Address', 'underscore', 'sprd/entity/Person'], function (SprdModel, AddressEntity, _, Person) {
 
+    var ADDRESS_TYPES = AddressEntity.ADDRESS_TYPES;
+
     /**
      * User Address shares the same interface and methods like Address but with some extensions
      *
@@ -26,19 +28,33 @@ define(['sprd/data/SprdModel', 'sprd/entity/Address', 'underscore', 'sprd/entity
         parse: AddressEntity.prototype.parse,
         compose: AddressEntity.prototype.compose,
         _commitChangedAttributes: AddressEntity.prototype._commitChangedAttributes,
+
         isPackStation: AddressEntity.prototype.isPackStation,
+        isPostfiliale: AddressEntity.prototype.isPostfiliale,
+        isPrivate: AddressEntity.prototype.isPrivate,
+        isDhl: AddressEntity.prototype.isDhl,
+        isType: AddressEntity.prototype.isType,
+        isUpsPickup: AddressEntity.prototype.isUpsPickup,
+
         needsCounty: AddressEntity.prototype.needsCounty,
         needsZipCode: AddressEntity.prototype.needsZipCode,
         isStateRequired: AddressEntity.prototype.isStateRequired,
         hasStates: AddressEntity.prototype.hasStates,
         isCompany: AddressEntity.prototype.isCompany,
+
         _commitType: function (type) {
-            this.set('billingAddress', type != AddressEntity.ADDRESS_TYPES.PACKSTATION);
+            var value = type != ADDRESS_TYPES.PACKSTATION && type != ADDRESS_TYPES.POSTFILIALE && type != ADDRESS_TYPES.UPS_PICKUP;
+            this.set('billingAddress', value);
+
+            if (!value) {
+                this.set('defaultBillingAddress', value);
+            }
         },
         _commitVatId: AddressEntity.prototype._commitVatId,
         _commitCountry: AddressEntity.prototype._commitCountry,
         _commitPersonSalutation: AddressEntity.prototype._commitPersonSalutation,
         supportsPackStation: AddressEntity.prototype.supportsPackStation,
+        supportsUpsPickup: AddressEntity.prototype.supportsUpsPickup,
         needsVatId: AddressEntity.prototype.needsVatId
     });
 
